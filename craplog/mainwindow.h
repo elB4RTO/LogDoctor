e3512b7dcee24ec9e7fdddb1d92832d553e85afe
Valentino Orlandi <valentinox@disroot.org>
2022-06-21 21:07:06 +0200

    Building interface for crapLOG

--- craplog/mainwindow.h
+++ craplog/mainwindow.h
@@ -1,21 +1,34 @@
-#ifndef CRAPLOG_H
-#define CRAPLOG_H
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
 #include <QMainWindow>
 
+#include "./window/craplog.h"
+#include "qtreewidget.h"
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class Craplog; }
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class Craplog : public QMainWindow
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
 public:
-    Craplog(QWidget *parent = nullptr);
-    ~Craplog();
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    
+private slots:
+    void on_buttonViewFile_clicked();
+    
+    void on_checkAllLogFiles_stateChanged(int arg1);
+    
+    void on_buttonRefreshList_clicked();
+    
+    void on_listLogFiles_itemDoubleClicked(QTreeWidgetItem *item, int column);
     
 private:
-    Ui::Craplog *ui;
+    Ui::MainWindow *ui;
+    Craplog craplog;
 };
-#endif // CRAPLOG_H
+#endif // MAINWINDOW_H
