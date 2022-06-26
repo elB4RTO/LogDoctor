f67df857936648a7234246154efab657af36f3bc
Valentino Orlandi <valentinox@disroot.org>
2022-06-26 17:38:35 +0200

    Moved LogOps from Utilities to Craplog modules

---
+++ craplog/tools/craplog/modules/logs.cpp
@@ -0,0 +1,19 @@
+
+#include "logs.h"
+
+
+LogOps::LogOps()
+{
+
+}
+
+std::vector<std::string> LogOps::splitLine( std::string line, int type )
+{
+
+}
+
+std::vector<std::string> LogOps::splitLines( std::string line, int type )
+{
+
+}
+
---
+++ craplog/tools/craplog/modules/logs.h
@@ -0,0 +1,22 @@
+#ifndef LOGS_H
+#define LOGS_H
+
+#include <string>
+#include <vector>
+
+
+class LogOps
+{
+public:
+    LogOps();
+
+    std::vector<std::string> splitLine( std::string line, int type );
+    std::vector<std::string> splitLines( std::string line, int type );
+
+private:
+    // types constants
+    const int ACCESS_LOGS=1, ERROR_LOGS=2;
+
+};
+
+#endif // LOGS_H
