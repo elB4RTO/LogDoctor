59fcc31b9cd77e88d92e4398a9ec9339dde07fdd
elB4RTO <elbarto@disroot.org>
2022-06-26 17:40:06 +0200

    Updated #includes

--- craplog/utilities/rtf.h
+++ craplog/utilities/rtf.h
@@ -5,14 +5,13 @@
 #include "qstring.h"
 
 #include "utilities/strings.h"
-#include "tools/craplog.h"
 
 class RichText
 {
 public:
     RichText();
 
-    static QString enrichLogs( std::string content, Craplog::LogsFormat format, int color_scheme, bool wide_lines );
+    static QString enrichLogs( std::string content, int logs_format, int color_scheme, bool wide_lines );
 };
 
 #endif // RICHTEXT_H
