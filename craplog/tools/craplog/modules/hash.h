fd0e2cf7698ec4c5405051d36209b4e1d38f5304
elB4RTO <elbarto@disroot.org>
2022-06-26 17:39:23 +0200

    Created HashOps in Craplog modules

---
+++ craplog/tools/craplog/modules/hash.h
@@ -0,0 +1,33 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+
+class HashOps
+{
+public:
+    HashOps();
+
+    // retrieve the lists from file
+    void readLists( std::string dir_path );
+
+    // returns the hash
+    std::string digestFile( std::string file_path );
+    // check if the given hash is from a file which has been used already
+    bool hasBeenUsed( std::string file_hash, int web_server_id );
+    // insert the given hash/es in the relative list
+    bool insertHash( std::string hash, int web_server_id );
+    bool insertHashes( std::vector<std::string> hashes, int web_server_id );
+
+private:
+    // id constants
+    const int APACHE_ID=1, NGINX_ID=2, IIS_ID=3;
+    // lists of used files' hashes
+    std::unordered_map<int, std::vector<std::string>> hashes;
+
+};
+
+#endif // HASHOPS_H
