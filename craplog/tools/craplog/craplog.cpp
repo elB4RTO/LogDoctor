2f20002fa4b74bd1a4a73f2d6db05ba91dd41962
Valentino Orlandi <valentinox@disroot.org>
2022-06-26 20:59:52 +0200

    Update following FormatOps

--- craplog/tools/craplog/craplog.cpp
+++ craplog/tools/craplog/craplog.cpp
@@ -10,24 +10,20 @@
 
 Craplog::Craplog()
 {
-    this->access_logs_format = Craplog::LogsFormat{
-        .format = Craplog::Format::Combined,
+    this->current_ALF = FormatOps::LogsFormat{
         .string = "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"",
         .initial = "",
         .final   = "\"",
         .separators = {" ", " ", " ", " \"", "\" ", " ", " \"", "\" \""},
         .fields = {"client","logname","user","date_time","request","response_code","http_size","referer","user_agent"}
     };
-    this->error_logs_format = Craplog::LogsFormat{
-        .format = Craplog::Format::Default,
+    this->current_ELF = FormatOps::LogsFormat{
         .string = "[%t] [%l] [pid %P] %F: %E: [client %a] %M",
         .initial = "[",
         .final   = "",
         .separators = {"] [", "] [pid", "] ", ": ", ": [client ", "] "},
         .fields = {"date_time","error_level","pid","source_file","os_err","client:port","error_message"}
     };
-
-    this->hashOps.readLists( this->configs_path );
 
     // apache2 access/error logs location
     this->logs_paths.emplace( 1, new std::unordered_map<int, std::string> );
@@ -43,7 +39,9 @@ Craplog::Craplog()
     this->logs_paths[3].emplace( 2, "C:\\Windows\\System32\\LogFiles\\HTTPERR" );
 
 
-    //this->readConfigs();
+    /*this->readConfigs();
+
+    this->hashOps.readLists( this->configs_path );*/
 
     this->scanLogsDir();
 }
@@ -110,32 +108,41 @@ int Craplog::setLogFileSelected( QString file_name )
 void Craplog::scanLogsDir()
 {
     this->logs_list.clear();
-    // iterate over entries in the logs folder
-    for ( const auto& dir_entry : std::filesystem::directory_iterator{this->logs_path}) {
-        // get the attributes
-        int size = dir_entry.file_size();
-        std::string path = dir_entry.path().string();
-        std::string name = dir_entry.path().filename().string();
-        // match only files having ".log." in their name
-        if ( dir_entry.is_regular_file() == false
-          || this->isFileNameValid( name ) == false ) {
-            continue;
+    int n=2;
+    auto logs_paths = this->logs_paths[ this->current_WS ];
+    if ( logs_paths[1] == logs_paths[2] ) {
+        // same dir for both access and error logs, loop only once
+        n=1;
+    }
+    for ( int i=0; i<n; i++ ) {
+        std::string logs_path = logs_paths[i];
+        // iterate over entries in the logs folder
+        for ( const auto& dir_entry : std::filesystem::directory_iterator{logs_path}) {
+            // get the attributes
+            int size = dir_entry.file_size();
+            std::string path = dir_entry.path().string();
+            std::string name = dir_entry.path().filename().string();
+            // match only files having ".log." in their name
+            if ( dir_entry.is_regular_file() == false
+              || this->isFileNameValid( name ) == false ) {
+                continue;
+            }
+            auto logfile =LogFile{
+                .selected = false,
+                .size = size,
+                .name = QString::fromStdString( name ),
+                .hash = this->hashOps.digestFile( path ),
+                .path = path,
+                .type = this->defineFileType( name, IOutils::readLines( path ) )
+            };
+            if ( logfile.type == Craplog::LogType::Failed ) {
+                // failed to get the log type
+                // error message displayed while defining as failed in logOps
+                continue;
+            }
+            // push in the list
+            this->logs_list.push_back( logfile );
         }
-        auto logfile =LogFile{
-            .selected = false,
-            .size = size,
-            .name = QString::fromStdString( name ),
-            .hash = this->hashes.digestFile( path );
-            .path = path,
-            .type = this->defineFileType( name, IOutils::readLines( path ) )
-        };
-        if ( logfile.type == Craplog::LogType::Failed ) {
-            // failed to get the log type
-            // error message displayed while defining as failed in logOps
-            continue;
-        }
-        // push in the list
-        this->logs_list.push_back( logfile );
     }
 }
 
@@ -178,32 +185,32 @@ Craplog::LogType Craplog::defineFileType( std::string name, std::vector<std::str
     real_type = Craplog::LogType::Failed;
     for ( const std::string& line : lines ) {
         if ( supposed_type == Craplog::LogType::Access ) {
-            if ( this->access_logs_format.initial.size() > 0 ) {
+            if ( this->current_ALF.initial.size() > 0 ) {
                 // a fixed starter char is set
-                if ( StringOps::startsWith( line, this->access_logs_format.initial ) == false ) {
+                if ( StringOps::startsWith( line, this->current_ALF.initial ) == false ) {
                     // but wasn't found
                     continue;
                 }
             }
-            if ( this->access_logs_format.final.empty() > 0 ) {
+            if ( this->current_ALF.final.size() > 0 ) {
                 // a fixed starter char is set
-                if ( StringOps::endsWith( line, this->access_logs_format.final ) == false ) {
+                if ( StringOps::endsWith( line, this->current_ALF.final ) == false ) {
                     // but wasn't found
                     continue;
                 }
             }
             n_acc++;
         } else {
-            if ( this->error_logs_format.initial.empty() == false ) {
+            if ( this->current_ELF.initial.size() > 0 ) {
                 // a fixed starter char is set
-                if ( StringOps::startsWith( line, this->error_logs_format.initial ) == false ) {
+                if ( StringOps::startsWith( line, this->current_ELF.initial ) == false ) {
                     // but wasn't found
                     continue;
                 }
             }
-            if ( this->error_logs_format.final.empty() == false ) {
+            if ( this->current_ELF.final.size() > 0 ) {
                 // a fixed starter char is set
-                if ( StringOps::endsWith( line, this->error_logs_format.final ) == false ) {
+                if ( StringOps::endsWith( line, this->current_ELF.final ) == false ) {
                     // but wasn't found
                     continue;
                 }
@@ -232,23 +239,46 @@ Craplog::LogType Craplog::defineFileType( std::string name, std::vector<std::str
 
 
 // get the logs format
-Craplog::LogsFormat Craplog::getAccessLogsFormat()
+FormatOps::LogsFormat Craplog::getAccessLogsFormat( int web_server_id )
 {
-    return this->access_logs_format;
+    return this->logs_formats[ web_server_id ][1];
 }
-Craplog::LogsFormat Craplog::getErrorLogsFormat()
+FormatOps::LogsFormat Craplog::getErrorLogsFormat( int web_server_id )
 {
-    return this->error_logs_format;
+    return this->logs_formats[ web_server_id ][2];
 }
 
 // set the logs format
-void Craplog::setAccessLogsFormat( std::string format_string )
+void Craplog::setAccessLogsFormat( int web_server_id, std::string format_string )
 {
 
 }
-void Craplog::setErrorLogsFormat( std::string format_string )
+void Craplog::setErrorLogsFormat( int web_server_id, std::string format_string )
 {
 
+}
+
+
+// set the current access logs format
+void Craplog::setCurrentALF()
+{
+    this->current_ALF = this->logs_formats[ this->current_WS ][1];
+}
+// set the current error logs format
+void Craplog::setCurrentELF()
+{
+    this->current_ELF = this->logs_formats[ this->current_WS ][2];
+}
+
+// get the current access logs format
+FormatOps::LogsFormat Craplog::getCurrentALF()
+{
+    return this->current_ALF;
+}
+// get the current error logs format
+FormatOps::LogsFormat Craplog::getCurrentELF()
+{
+    return this->current_ELF;
 }
 
 
--- craplog/tools/craplog/craplog.h
+++ craplog/tools/craplog/craplog.h
@@ -6,6 +6,7 @@
 
 #include <QMainWindow>
 
+#include "tools/craplog/modules/formats.h"
 #include "tools/craplog/modules/hash.h"
 #include "tools/craplog/modules/logs.h"
 
@@ -15,25 +16,20 @@ class Craplog
 public:
     Craplog();
 
+    // web servers
+    const unsigned int APACHE_ID=11, NGINX_ID=12, IIS_ID=13;
+
     // logs formats
-    enum Format{
-        Default=10,
-        Common=11,
-        Combined=12,
-        Custom=13
-    };
-    struct LogsFormat {
-        Format format;
-        std::string string;
-        std::string initial;
-        std::string final;
-        std::vector<std::string> separators;
-        std::vector<std::string> fields;
-    };
-    void setAccessLogsFormat( std::string format_string );
-    void setErrorLogsFormat( std::string format_string );
-    LogsFormat getAccessLogsFormat();
-    LogsFormat getErrorLogsFormat();
+    // for settings
+    void setAccessLogsFormat( int web_server_id, std::string format_string );
+    void setErrorLogsFormat( int web_server_id, std::string format_string );
+    FormatOps::LogsFormat getAccessLogsFormat( int web_server_id );
+    FormatOps::LogsFormat getErrorLogsFormat( int web_server_id );
+    // currently used
+    void setCurrentWSID( int web_server_id );
+    int  getCurrentWSID();
+    FormatOps::LogsFormat getCurrentALF();
+    FormatOps::LogsFormat getCurrentELF();
 
 
     // log file infoes
@@ -72,7 +68,10 @@ public:
 
 private:
     // configs location
-    std::string configs_path="~/.config/craplog";
+    std::string configs_path;
+
+    // web server related
+    int current_WS = 11;
 
     // logs related
     std::unordered_map<int, std::unordered_map<int, std::string>> logs_base_names;
@@ -82,8 +81,11 @@ private:
     void scanLogsDir();
 
     // logs format related
-    LogsFormat access_logs_format;
-    LogsFormat error_logs_format;
+    std::unordered_map<int, std::unordered_map<int, FormatOps::LogsFormat>> logs_formats;
+    void setCurrentALF();
+    void setCurrentELF();
+    FormatOps::LogsFormat current_ALF;
+    FormatOps::LogsFormat current_ELF;
 
     void readConfigs();
 
