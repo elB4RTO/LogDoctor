1d1c87b078c4fa013f8e74093bb704170e11c11d
elB4RTO <elbarto@disroot.org>
2022-06-27 19:50:18 +0200

    Code improvements, updates

--- craplog/tools/craplog/craplog.h
+++ craplog/tools/craplog/craplog.h
@@ -19,13 +19,12 @@ public:
     // web servers ID constants
     const unsigned int APACHE_ID=11, NGINX_ID=12, IIS_ID=13;
 
-    // logs formats
-    // for settings
+    // logs formats web server specific settings
     void setAccessLogsFormat( int web_server_id, std::string format_string );
     void setErrorLogsFormat( int web_server_id, std::string format_string );
     FormatOps::LogsFormat getAccessLogsFormat( int web_server_id );
     FormatOps::LogsFormat getErrorLogsFormat( int web_server_id );
-    // currently used
+    // currently used WS and LF
     void setCurrentWSID( int web_server_id );
     int  getCurrentWSID();
     FormatOps::LogsFormat getCurrentALF();
@@ -49,10 +48,13 @@ public:
     LogFile getLogFileItem( QString file_name );
     std::string getLogFilePath( QString file_name );
     // set a file in the list as selected, to be used for the stats
-    int setLogFileSelected( QString file_name );
+    bool setLogFileSelected( QString file_name );
     // check if a file name respects the one set
     bool isFileNameValid( std::string name );
 
+    // warning file size (in Bytes)
+    int getWarningSize();
+    void setWarningSize( int new_size );
     // logs usage control
     HashOps hashOps;
 
@@ -60,6 +62,9 @@ public:
     LogOps logOps;
 
 private:
+    // quantoty of informational dialogs to display
+    int dialogs_Level = 1; // 0: essential, 1: usefull, 2: explanatory
+
     // configs location
     std::string configs_path;
 
@@ -67,10 +72,12 @@ private:
     int current_WS = 11;
 
     // logs related
+    int warning_size = 1'048'576; //104857600; // in Bytes ( => 100 MiB )
+    // web server specific paths and file names
     std::unordered_map<int, std::unordered_map<int, std::string>> logs_base_names;
     std::unordered_map<int, std::unordered_map<int, std::string>> logs_paths;
-    std::vector<LogFile> logs_list;
     // logs list related
+    std::vector<LogFile> logs_list;
     void scanLogsDir();
 
     // logs format related
