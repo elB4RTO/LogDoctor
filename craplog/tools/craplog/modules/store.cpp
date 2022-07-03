eac718711acf500b7832b50dd9e715e81788f899
Valentino Orlandi <valentinox@disroot.org>
2022-07-03 19:09:51 +0200

    Added new Craplog module StoreOps

---
+++ craplog/tools/craplog/modules/store.cpp
@@ -0,0 +1,21 @@
+#include "store.h"
+
+StoreOps::StoreOps()
+{/*
+    this->logs_fields[11][1][1]  = "year";
+    this->logs_fields[11][1][2]  = "month";
+    this->logs_fields[11][1][3]  = "day";
+    this->logs_fields[11][1][4]  = "hour";
+    this->logs_fields[11][1][5]  = "minute";
+    this->logs_fields[11][1][10] = "request_method";
+    this->logs_fields[11][1][11] = "request_page";
+    this->logs_fields[11][1][12] = "request_query";
+    this->logs_fields[11][1][13] = "response_code";
+    this->logs_fields[11][1][14] = "time_taken";
+    this->logs_fields[11][1][15] = "bytes_sent";
+    this->logs_fields[11][1][16] = "bytes_received";
+    this->logs_fields[11][1][17] = "referrer";
+    this->logs_fields[11][1][20] = "ip";
+    this->logs_fields[11][1][21] = "user_agent";
+    this->logs_fields[11][1][22] = "cookie";
+*/}
