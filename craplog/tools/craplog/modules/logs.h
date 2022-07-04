7e73550ad72035a685b0464d6a04ea3b5d5d128e
Valentino Orlandi <valentinox@disroot.org>
2022-07-04 21:03:09 +0200

    Improved log lines parsing

--- craplog/tools/craplog/modules/logs.h
+++ craplog/tools/craplog/modules/logs.h
@@ -28,14 +28,16 @@ public:
         std::unordered_map<int, FormatOps::LogsFormat>& format
         );
 
-    std::unordered_map<int, std::string> parseLine(
-        const std::string& line,
-        const FormatOps::LogsFormat& format
-        ) const;
+    // parse log lines to get data
     std::vector<std::unordered_map<int, std::string>> parseLines(
         const std::vector<std::string>& lines,
         const FormatOps::LogsFormat& format
-        ) const;
+        );
+
+    void resetPerfData();
+    // share perf data with craplog
+    int getSize(),
+        getLines();
 
 private:
     // types constants
@@ -51,6 +53,15 @@ private:
         const std::string& line,
         const FormatOps::LogsFormat& access_format,
         const FormatOps::LogsFormat& error_format );
+
+    // job related
+    std::unordered_map<int, std::string> parseLine(
+        const std::string& line,
+        const FormatOps::LogsFormat& format
+        );
+    // temporary vars
+    int size, lines;
+
 
 };
 
