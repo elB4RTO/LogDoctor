5d077ed8b3823f4f4d6bafa7ab3f0b453e40c04b
Valentino Orlandi <valentinox@disroot.org>
2022-07-05 22:01:26 +0200

    Implemented storeData method

--- craplog/tools/craplog/modules/store.h
+++ craplog/tools/craplog/modules/store.h
@@ -1,11 +1,15 @@
 #ifndef STORE_H
 #define STORE_H
 
+#include "tools/craplog/craplog.h"
+
 
 class StoreOps
 {
 public:
     StoreOps();
+
+    static bool storeData( sqlite3* db, Craplog& caplog, const std::vector<std::unordered_map<int, std::string>>& data, const int log_type );
 };
 
 #endif // STORE_H
