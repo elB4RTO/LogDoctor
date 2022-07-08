9b48e2ac050df008e691bd1458d5d7b812ec6a19
elB4RTO <elbarto@disroot.org>
2022-07-08 21:34:41 +0200

    Replaced 'sqlite3' library with 'QSql' plugin
    Issues linking sqlite library to project and makefile, replaced with Qt
    plugin.
    New method 'newStatsDatabase' to create a new database file for the
    statistics.
    Updated 'checkStatsDatabase' method.

--- craplog/modules/checks.h
+++ craplog/modules/checks.h
@@ -9,10 +9,14 @@ class CheckSec
 public:
     CheckSec();
 
-    static bool checkStatsDatabase( std::string path );
-    static bool checkHashesDatabase( std::string path );
+    static bool checkStatsDatabase( const std::string& db_path );
+    static bool checkHashesDatabase( const std::string& db_path );
 
-    static bool checkCraplog( Craplog craplog );
+    static bool checkCraplog( const Craplog& craplog );
+
+
+private:
+    static bool newStatsDatabase( QSqlDatabase& db, const std::string& db_path, const QString& db_name, const std::vector<QString>& ws_names );
 };
 
 #endif // CHECKS_H
