9628ac24e1ada16310b73b704acdfa793b119511
Valentino Orlandi <valentinox@disroot.org>
2022-07-09 18:21:46 +0200

    Added new methods
    Added dialog_leel variable and relative setter method.
    Morphed readList into loadUsedHashesLists and implemented to read from
    SQLite database.
    Improved insertUsedHash/es to store them into the database.

--- craplog/tools/craplog/modules/hash.h
+++ craplog/tools/craplog/modules/hash.h
@@ -5,26 +5,45 @@
 #include <vector>
 #include <unordered_map>
 
+#include <QVariant>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+
 
 class HashOps
 {
 public:
     HashOps();
 
-    // retrieve the lists from file
-    void readLists( const std::string& dir_path );
+    void setDialogLevel( const int new_level );
+
+    // retrieve the lists from the database file
+    bool loadUsedHashesLists( const std::string& db_path );
 
     // returns the hash
     std::string digestFile( const std::string& file_path );
     // check if the given hash is from a file which has been used already
     bool hasBeenUsed( const std::string& file_hash, const int web_server_id, const int log_type );
     // insert the given hash/es in the relative list
-    bool insertUsedHash( const std::string& hash, const int web_server_id, const int log_type );
-    bool insertUsedHashes( const std::unordered_map<int, std::vector<std::string>>& hashes, const int web_server_id );
+    bool insertUsedHash( QSqlDatabase& db, QSqlQuery& query, const QString& db_name, const std::string& hash, const int web_server_id, const int log_type );
+    bool insertUsedHashes( const std::string& db_path, const std::unordered_map<int, std::vector<std::string>>& hashes, const int web_server_id );
 
 private:
+    int dialog_level = 2;
+
     // id constants
     const int APACHE_ID=11, NGINX_ID=12, IIS_ID=13;
+
+    const std::unordered_map<int, QString> ws_names = {
+        {11, "apache"},
+        {12, "nginx"},
+        {13, "iis"} };
+    const std::unordered_map<int, QString> log_types = {
+        {1, "access"},
+        {2, "error"} };
+
+
     // lists of used files' hashes
     std::unordered_map<int, std::unordered_map<int, std::vector<std::string>>> hashes;
 
