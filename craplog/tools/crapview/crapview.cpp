6c78a04e2cb9773c285b8d7696525143bd9076e4
Valentino Orlandi <valentinox@disroot.org>
2022-07-20 21:43:19 +0200

    Added new tool CrapView

---
+++ craplog/tools/crapview/crapview.cpp
@@ -0,0 +1,9 @@
+#include "crapview.h"
+
+Crapview::Crapview()
+{
+    count_ws = 11, count_type = 1, count_field = 10,
+    daytime_ws = 11, daytime_type = 1, daytime_field = 10,
+    relational_ws = 11, relational_field_1 = 10, relational_field_2 = 11,
+    speed_ws = 11;
+}
