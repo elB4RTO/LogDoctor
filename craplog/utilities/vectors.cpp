b40508f403aafb700ee885ffbf6cbb9ad71e3198
Valentino Orlandi <valentinox@disroot.org>
2022-07-20 23:00:13 +0200

    Added alternative 'Contains' method

--- craplog/utilities/vectors.cpp
+++ craplog/utilities/vectors.cpp
@@ -8,6 +8,20 @@ VecOps::VecOps()
 }
 
 
+
+bool VecOps::contains( const std::vector<int>& list, const int flag )
+{
+    bool result = false;
+    for ( const int& item : list ) {
+        if ( item == flag ) {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+
 bool VecOps::contains( const std::vector<std::string>& list, const std::string& flag )
 {
     bool result = false;
