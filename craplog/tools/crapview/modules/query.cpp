ae5e53e0284c8e585cde6b777da96704b427540c
elB4RTO <elbarto@disroot.org>
2022-07-22 22:54:18 +0200

    Improvements + implementations
    Grouped maps elements for translation purposes.
    Implemented new method 'getItemsCount' for the 'StatsCount' tab chart.
    Started implementing new method 'getDaytimeCounts' for the 'StatsDay'
    tab chart.

--- craplog/tools/crapview/modules/query.cpp
+++ craplog/tools/crapview/modules/query.cpp
@@ -23,18 +23,16 @@ void DbQuery::setDialogLevel( const int new_level )
 void DbQuery::setDbPath( const std::string& path )
 {
     this->db_path = path;
+    this->db_name = QString::fromStdString( this->db_path.substr( this->db_path.find_last_of( '/' ) + 1 ) );
 }
 
 
-//select distinct hour from apache_access where year=2021 and month=6;
-
+// get a fresh map of available dates
 const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::vector<int>>>>>> DbQuery::refreshDates()
 {
     bool successful = true;
     std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::vector<int>>>>> dates = {
         {11,{ {1,{}},{2,{}} }}, {12,{ {1,{}},{2,{}} }}, {13,{ {1,{}},{2,{}} }} };
-
-    QString db_name = QString::fromStdString( this->db_path.substr( this->db_path.find_last_of( '/' ) + 1 ) );
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName( QString::fromStdString( this->db_path ));
@@ -46,7 +44,7 @@ const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<in
         if ( this->dialog_level == 2 ) {
             err_msg = db.lastError().text();
         }
-        DialogSec::errDatabaseFailedOpening( nullptr, db_name, err_msg );
+        DialogSec::errDatabaseFailedOpening( nullptr, this->db_name, err_msg );
 
     } else {
         // recursively query years, months and days for every WebServer
@@ -72,7 +70,7 @@ const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<in
             if ( Y_query.exec( QString("SELECT DISTINCT year FROM %1 ORDER BY year ASC;").arg(tbl) ) == false ) {
                 // error querying database
                 successful = false;
-                DialogSec::errDatabaseFailedExecuting( nullptr, db_name, Y_query.lastQuery(), Y_query.lastError().text() );
+                DialogSec::errDatabaseFailedExecuting( nullptr, this->db_name, Y_query.lastQuery(), Y_query.lastError().text() );
                 break;
 
             } else {
@@ -99,7 +97,7 @@ const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<in
                     if ( M_query.exec( QString("SELECT DISTINCT month FROM %1 WHERE year=%2 ORDER BY month ASC;").arg(tbl).arg(year) ) == false ) {
                         // error querying database
                         successful = false;
-                        DialogSec::errDatabaseFailedExecuting( nullptr, db_name, M_query.lastQuery(), M_query.lastError().text() );
+                        DialogSec::errDatabaseFailedExecuting( nullptr, this->db_name, M_query.lastQuery(), M_query.lastError().text() );
                         break;
 
                     } else {
@@ -125,7 +123,7 @@ const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<in
                             if ( D_query.exec( QString("SELECT DISTINCT day FROM %1 WHERE year=%2 AND month=%3 ORDER BY day ASC;").arg(tbl).arg(year).arg(month) ) == false ) {
                                 // error querying database
                                 successful = false;
-                                DialogSec::errDatabaseFailedExecuting( nullptr, db_name, D_query.lastQuery(), D_query.lastError().text() );
+                                DialogSec::errDatabaseFailedExecuting( nullptr, this->db_name, D_query.lastQuery(), D_query.lastError().text() );
                                 break;
 
                             } else {
@@ -174,17 +172,238 @@ const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<in
     return std::make_tuple( successful, dates );
 }
 
-/*const std::vector<QString> DbQuery::getYears( const QString& web_server, const QString& log_type )
-{
 
+// get, group and count items of a specific field in a date
+const std::tuple<bool, std::vector<std::tuple<QString, int>>> DbQuery::getItemsCount(const QString& web_server, const QString& log_type, const QString& year, const QString& month, const QString& day, const QString& log_field )
+{
+    bool successful = true;
+    QHash<QString, int> aux_items;
+    std::vector<std::tuple<QString, int>> items;
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName( QString::fromStdString( this->db_path ));
+
+    if ( db.open() == false ) {
+        // error opening database
+        successful = false;
+        QString err_msg = "";
+        if ( this->dialog_level == 2 ) {
+            err_msg = db.lastError().text();
+        }
+        DialogSec::errDatabaseFailedOpening( nullptr, this->db_name, err_msg );
+
+    } else {
+        QString table;
+        if ( web_server == "Apache2" ) {
+            table += "apache_";
+        } else if ( web_server == "Nginx" ) {
+            table += "nginx_";
+        } else if ( web_server == "Apache2" ) {
+            table += "iis_";
+        } else {
+            // unexpected WebServer
+            successful = false;
+            DialogSec::errGeneric( nullptr, QString("Unexpected WebServer:\n%1").arg( web_server ), true );
+        }
+        if ( successful == true ) {
+            if ( log_type == "Access" ) {
+                table += "access";
+            } else if ( web_server == "Error" ) {
+                table += "error";
+            } else {
+                // unexpected LogType
+                successful = false;
+                DialogSec::errGeneric( nullptr, QString("Unexpected LogType:\n%1").arg( web_server ), true );
+            }
+        }
+        if ( successful == true ) {
+            // build the query statement
+            QSqlQuery query = QSqlQuery( db );
+            QString stmt = QString("SELECT %1 FROM %2 WHERE year=%3 AND month=%4 AND day=%5;")
+                .arg( this->LogFields_to_DbFields.value( log_field ))
+                .arg( table )
+                .arg( year )
+                .arg( this->Months_s2i.value( month ) )
+                .arg( day );
+            // quary the database
+            if ( query.exec( stmt.replace("'","''") ) == false ) {
+                // error querying database
+                successful = false;
+                DialogSec::errDatabaseFailedExecuting( nullptr, this->db_name, query.lastQuery(), query.lastError().text() );
+
+            } else {
+                try {
+                    // get data
+                    QString item;
+                    while ( query.next() ) {
+                        item = query.value(0).toString();
+                        if ( item.size() > 0 ) {
+                            if ( aux_items.find( item ) == aux_items.end() ) {
+                                // not present yet
+                                aux_items.insert( item, 1 );
+                            } else {
+                                // already in the list, increase the count
+                                aux_items[ item ] ++;
+                            }
+                        }
+                    }
+                } catch (...) {
+                    // something failed
+                    successful = false;
+                    DialogSec::errGeneric( nullptr, QString("An error occured while processing"), true );
+                }
+            }
+        }
+        if ( successful == true ) {
+            // sort the list
+            std::tuple<QString, int> item;
+            // morph tha HashMap into a Vector of Tuples
+            for ( const QString& v : aux_items.keys() ) {
+                item = std::make_tuple( v, aux_items.value( v ) );
+                items.push_back( item );
+            }
+            // sort the vector
+            for ( int i=0; i<items.size(); i++ ) {
+                for ( int j=0; j<items.size(); j++ ) {
+                    if ( std::get<1>(items.at( i )) > std::get<1>(items.at( j )) ) {
+                        item = items.at( i );
+                        items.at( i ) = items.at( j );
+                        items.at( j ) = item;
+                    }
+                }
+            }
+        }
+        aux_items.clear();
+    }
+
+    if ( successful == false ) {
+        items.clear();
+    }
+    if ( db.isOpen() == true ) {
+        db.close();
+    }
+    return std::make_tuple( successful, items );
 }
 
-const std::vector<QString> DbQuery::getMonths( const QString& web_server, const QString& log_type, const QString& year )
-{
 
+const std::tuple<bool, std::unordered_map<int, std::unordered_map<int, int>>> DbQuery::getDaytimeCounts(const QString& web_server, const QString& log_type, const QString& from_year_, const QString& from_month_, const QString& from_day_, const QString& to_year_, const QString& to_month_, const QString& to_day_, const QString& log_field, const QString& field_filter )
+{
+    bool successful = true;
+    std::unordered_map<int, std::unordered_map<int, int>> data = {
+        {0,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {1,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {2,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {3,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {4,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {5,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {6,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {7,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {8,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {9,  {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {10, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {11, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {12, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {13, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {14, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {15, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {16, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {17, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {18, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {19, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {20, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {21, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+        {22, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},  {23, {{10,0},{20,0},{30,0},{40,0},{50,0},{60,0}}},
+    };
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName( QString::fromStdString( this->db_path ));
+
+    if ( db.open() == false ) {
+        // error opening database
+        successful = false;
+        QString err_msg = "";
+        if ( this->dialog_level == 2 ) {
+            err_msg = db.lastError().text();
+        }
+        DialogSec::errDatabaseFailedOpening( nullptr, this->db_name, err_msg );
+
+    } else {
+        QString table;
+        if ( web_server == "Apache2" ) {
+            table += "apache_";
+        } else if ( web_server == "Nginx" ) {
+            table += "nginx_";
+        } else if ( web_server == "Apache2" ) {
+            table += "iis_";
+        } else {
+            // unexpected WebServer
+            successful = false;
+            DialogSec::errGeneric( nullptr, QString("Unexpected WebServer:\n%1").arg( web_server ), true );
+        }
+        if ( successful == true ) {
+            if ( log_type == "Access" ) {
+                table += "access";
+            } else if ( web_server == "Error" ) {
+                table += "error";
+            } else {
+                // unexpected LogType
+                successful = false;
+                DialogSec::errGeneric( nullptr, QString("Unexpected LogType:\n%1").arg( web_server ), true );
+            }
+        }
+        int from_year, from_month, from_day,
+            to_year, to_month, to_day;
+        if ( successful == true ) {
+            // setup period limits
+            try {
+                from_year  = from_year_.toInt();
+                from_month = this->Months_s2i.value( from_month_ );
+                from_day   = from_day_.toInt();
+                to_year  = ( to_year_.size() == 0 )  ? from_year  : to_year_.toInt() ;
+                to_month = ( to_month_.size() == 0 ) ? from_month : this->Months_s2i.value( to_month_ ) ;
+                to_day   = ( to_day_.size() == 0 )   ? from_day   : to_day_.toInt() ;
+            } catch (...) {
+                // failed to convert to integers
+                successful = false;
+                DialogSec::errGeneric( nullptr, QString("An error occured while processing dates"), true );
+            }
+        }
+        if ( successful == true ) {
+            // build the query statement
+            QSqlQuery query = QSqlQuery( db );
+            QString stmt;
+
+            // select years
+            for ( int year=from_year; year<=to_year; year++ ) {
+                stmt = QString("SELECT %1, year, month, day, hour, minute FROM %2 WHERE year=%3")
+                    .arg( this->LogFields_to_DbFields.value( log_field ))
+                    .arg( table )
+                    .arg( year );
+
+                for ( int month=from_month; month<=to_month; month++ ) {
+
+
+                }
+
+
+                // quary the database
+                if ( query.exec( stmt.replace("'","''") ) == false ) {
+                    // error querying database
+                    successful = false;
+                    DialogSec::errDatabaseFailedExecuting( nullptr, this->db_name, query.lastQuery(), query.lastError().text() );
+
+                } else {
+                    try {
+                        // get data
+                        while ( query.next() ) {
+
+                        }
+                    } catch (...) {
+                        // something failed
+                        successful = false;
+                        DialogSec::errGeneric( nullptr, QString("An error occured while processing"), true );
+                    }
+                }
+            }
+        }
+    }
+
+
+    if ( successful == false ) {
+        data.clear();
+    }
+    if ( db.isOpen() == true ) {
+        db.close();
+    }
+    return std::make_tuple( successful, data );
 }
 
-const std::vector<QString> DbQuery::getDays( const QString& web_server, const QString& log_type, const QString& year, const QString& month )
-{
-
-}*/
--- craplog/tools/crapview/modules/query.h
+++ craplog/tools/crapview/modules/query.h
@@ -1,10 +1,30 @@
 #ifndef QUERY_H
 #define QUERY_H
 
+#include <QMessageBox>
 #include <QString>
+#include <QHash>
 
 #include <vector>
 #include <unordered_map>
+
+
+const QHash<int, QString>
+    TYPES = {
+        {1,QMessageBox::tr("Access")}, {2,QMessageBox::tr("Error")} },
+    FIELDS = {
+        {10,QMessageBox::tr("Protocol")},   {11,QMessageBox::tr("Method")},
+        {12,QMessageBox::tr("URI")},        {13,QMessageBox::tr("Query")},      {14,QMessageBox::tr("Response code")},
+        {15,QMessageBox::tr("Time taken")}, {16,QMessageBox::tr("Bytes sent")}, {17,QMessageBox::tr("Bytes received")},
+        {18,QMessageBox::tr("Referrer")},   {22,QMessageBox::tr("Cookie")},
+        {20,QMessageBox::tr("Client")},     {21,QMessageBox::tr("User-agent")},
+        {30,QMessageBox::tr("Port")},
+        {31,QMessageBox::tr("Level")}, {32,QMessageBox::tr("Message")}, {33,QMessageBox::tr("Source file")} },
+    MONTHS = {
+        {1, QMessageBox::tr("January")},  {2, QMessageBox::tr("February")},  {3, QMessageBox::tr("March")},
+        {4, QMessageBox::tr("April")},    {5, QMessageBox::tr("May")},       {6, QMessageBox::tr("June")},
+        {7, QMessageBox::tr("July")},     {8, QMessageBox::tr("August")},    {9, QMessageBox::tr("September")},
+        {10,QMessageBox::tr("October")},  {11,QMessageBox::tr("November")},  {12,QMessageBox::tr("December")} };
 
 
 class DbQuery
@@ -19,15 +39,50 @@ public:
 
     const std::tuple<const bool, const std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::vector<int>>>>>> refreshDates();
 
-    /*const std::vector<QString> getYears( const QString& web_server, const QString& log_type );
-    const std::vector<QString> getMonths( const QString& web_server, const QString& log_type, const QString& year );
-    const std::vector<QString> getDays( const QString& web_server, const QString& log_type, const QString& year, const QString& month );*/
+    const std::tuple<bool, std::vector<std::tuple<QString, int>>> getItemsCount(
+        const QString& web_server, const QString& log_type,
+        const QString& year, const QString& month, const QString& day,
+        const QString& log_field );
+
+    const std::tuple<bool, std::unordered_map<int, std::unordered_map<int, int>>> getDaytimeCounts(
+        const QString& web_server, const QString& log_type,
+        const QString& from_year_, const QString& from_month_, const QString& from_day_,
+        const QString& to_year_, const QString& to_month_, const QString& to_day_,
+        const QString& log_field, const QString& field_filter );
 
 private:
     // quantity of informational dialogs to display
     int dialog_level = 2; // 0: essential, 1: usefull, 2: explanatory
 
     std::string db_path;
+    QString db_name;
+
+    const QHash<QString, QString>
+        LogFields_to_DbFields = {
+            {FIELDS.value(10), "protocol"},
+            {FIELDS.value(11), "method"},
+            {FIELDS.value(12), "request"},
+            {FIELDS.value(13), "query"},
+            {FIELDS.value(14), "response"},
+            {FIELDS.value(15), "time_taken"},
+            {FIELDS.value(16), "bytes_sent"},
+            {FIELDS.value(17), "bytes_received"},
+            {FIELDS.value(18), "referrer"},
+            {FIELDS.value(20), "client"},
+            {FIELDS.value(21), "user_agent"},
+            {FIELDS.value(22), "cookie"},
+            {FIELDS.value(30), "port"},
+            {FIELDS.value(31), "level"},
+            {FIELDS.value(32), "message"},
+            {FIELDS.value(33), "source_file"}
+        };
+
+    const QHash<QString, int>
+        Months_s2i = {
+            {MONTHS.value(1),1},   {MONTHS.value(2),2},   {MONTHS.value(3),3},
+            {MONTHS.value(4),4},   {MONTHS.value(5),5},   {MONTHS.value(6),6},
+            {MONTHS.value(7),7},   {MONTHS.value(9),8},   {MONTHS.value(9),9},
+            {MONTHS.value(10),10}, {MONTHS.value(11),11}, {MONTHS.value(12),12} };
 
     /*const std::unordered_map<std::string, int>
         WebServer_str2int = {
