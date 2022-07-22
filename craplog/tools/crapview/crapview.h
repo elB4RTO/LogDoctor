14cd9705868b0c2cf7d916424f6d259454586e7e
elB4RTO <elbarto@disroot.org>
2022-07-22 22:49:51 +0200

    Improvements + started implementing stats charts

--- craplog/tools/crapview/crapview.h
+++ craplog/tools/crapview/crapview.h
@@ -2,6 +2,8 @@
 #define CRAPVIEW_H
 
 #include <QHash>
+#include <QtCharts>
+#include <QTableWidget>
 
 #include "tools/crapview/modules/query.h"
 
@@ -18,6 +20,8 @@ public:
 
     void setDbPath( const std::string& path );
 
+    int getMonthNumber( const QString& month_str );
+
     void refreshDates();
 
     const QStringList getYears( const QString& web_server, const QString& logs_type );
@@ -25,6 +29,34 @@ public:
     const QStringList getDays( const QString& web_server, const QString& logs_type, const QString& year, const QString& month );
 
     const QStringList getFields( const QString& tab, const QString& logs_type );
+
+    void drawSpeed(
+        QTableWidget* table, QtCharts::QChartView* chart,
+         const std::unordered_map<std::string, QFont>& fonts,
+        const QString& web_server,
+        const QString& year, const QString& month, const QString& day,
+        const QString& protocol, const QString& method, const QString& uri, const QString& query, const QString& response );
+
+    void drawCount(
+        QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts,
+        const QString& web_server, const QString& log_type,
+        const QString& year, const QString& month, const QString& day,
+        const QString& field );
+
+    void drawDay(
+        QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts,
+        const QString& web_server, const QString& log_type,
+        const QString& from_year, const QString& from_month, const QString& from_day,
+        const QString& to_year, const QString& to_month, const QString& to_day,
+        const QString& field, const QString& filter );
+
+    void drawRelat(
+        QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts,
+        const QString& web_server, const QString& log_type,
+        const QString& from_year, const QString& from_month, const QString& from_day,
+        const QString& to_year, const QString& to_month, const QString& to_day,
+        const QString& field_1, const QString& filter_1,
+        const QString& field_2, const QString& filter_2 );
 
 
 private:
@@ -41,11 +73,11 @@ private:
     // { tab : { log_type_str : [ fields ] } }
     const QHash<QString, QHash<QString, QStringList>> fields = {
         {"Daytime", {
-            {"Access", {"Protocol","Method","URI","Query","Response code","Referrer","Cookie","User-agent","Client"}},
-            {"Error",  {"Level","Message","Source file","Client","Port"}} }},
+            {TYPES.value(1),  {FIELDS.value(10),FIELDS.value(11),FIELDS.value(12),FIELDS.value(13),FIELDS.value(14),FIELDS.value(18),FIELDS.value(22),FIELDS.value(21),FIELDS.value(20)}},
+            {TYPES.value(2),  {FIELDS.value(31),FIELDS.value(32),FIELDS.value(33),FIELDS.value(20),FIELDS.value(30)}} }},
         {"Relational", {
-            {"Access", {"Protocol","Method","URI","Query","Response code","Time taken","Bytes sent","Bytes received","Referrer","Cookie","User-agent","Client"}},
-            {"Error",  {"Level","Message","Source file","Client","Port"}} }}
+            {TYPES.value(1),  {FIELDS.value(10),FIELDS.value(11),FIELDS.value(12),FIELDS.value(13),FIELDS.value(14),FIELDS.value(15),FIELDS.value(16),FIELDS.value(17),FIELDS.value(18),FIELDS.value(22),FIELDS.value(21),FIELDS.value(20)}},
+            {TYPES.value(2),  {FIELDS.value(31),FIELDS.value(32),FIELDS.value(33),FIELDS.value(20),FIELDS.value(30)}} }}
     };
 
     // conversion between text and IDs
@@ -53,10 +85,12 @@ private:
         WebServer_s2i = {
             {"Apache2",11}, {"Nginx",12}, {"IIS",13} },
         LogsType_s2i  = {
-            {"Access",1}, {"Error",2} },
+            {TYPES.value(1),1}, {TYPES.value(2),2} },
         Months_s2i = {
-            {"January",1}, {"February",2}, {"March",3}, {"April",4}, {"May",5}, {"June",6},
-            {"July",7}, {"August",8}, {"September",9}, {"October",10}, {"November",11}, {"December",12} };
+            {MONTHS.value(1),1},   {MONTHS.value(2),2},   {MONTHS.value(3),3},
+            {MONTHS.value(4),4},   {MONTHS.value(5),5},   {MONTHS.value(6),6},
+            {MONTHS.value(7),7},   {MONTHS.value(9),8},   {MONTHS.value(9),9},
+            {MONTHS.value(10),10}, {MONTHS.value(11),11}, {MONTHS.value(12),12} };
 
     const QHash<int, QString>
         /*WebServer_i2s = {
@@ -70,8 +104,10 @@ private:
             {20,"client"}, {21,"user_agent"}, {22,"cookie"},
             {30,"port"}, {31,"level"}, {32,"message"}, {33,"source_file"} },*/
         Months_i2s = {
-            {1,"January"}, {2,"February"}, {3,"March"}, {4,"April"}, {5,"May"}, {6,"June"},
-            {7,"July"}, {8,"August"}, {9,"September"}, {10,"October"}, {11,"November"}, {12,"December"} };
+            {1,MONTHS.value(1)},   {2,MONTHS.value(2)},   {3,MONTHS.value(3)},
+            {4,MONTHS.value(4)},   {5,MONTHS.value(5)},   {6,MONTHS.value(6)},
+            {7,MONTHS.value(7)},   {8,MONTHS.value(8)},   {9,MONTHS.value(9)},
+            {10,MONTHS.value(10)}, {11,MONTHS.value(11)}, {12,MONTHS.value(12)} };
 };
 
 #endif // CRAPVIEW_H
