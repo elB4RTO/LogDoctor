2a40e3142f353ef45402ef4cae2213bfd699956d
Valentino Orlandi <valentinox@disroot.org>
2022-07-22 22:49:51 +0200

    Improvements + started implementing stats charts

--- craplog/tools/crapview/crapview.cpp
+++ craplog/tools/crapview/crapview.cpp
@@ -35,6 +35,12 @@ void Crapview::refreshDates()
 }
 
 
+int Crapview::getMonthNumber( const QString& month_str )
+{
+    return this->Months_s2i.value( month_str );
+}
+
+
 
 const QStringList Crapview::getYears( const QString& web_server, const QString& logs_type )
 {
@@ -64,4 +70,86 @@ const QStringList Crapview::getDays( const QString& web_server, const QString& l
 const QStringList Crapview::getFields( const QString& tab, const QString& logs_type )
 {
     return this->fields.value( tab ).value( logs_type );
+}
+
+
+////////////////
+//// CHARTS ////
+////////////////
+void Crapview::drawSpeed(QTableWidget* table, QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts, const QString& web_server, const QString& year, const QString& month, const QString& day, const QString& protocol, const QString& method, const QString& uri, const QString& query, const QString& response )
+{
+
+}
+
+
+
+void Crapview::drawCount(QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts, const QString& web_server, const QString& log_type, const QString& year, const QString& month, const QString& day, const QString& field )
+{
+    auto result = this->dbQuery.getItemsCount( web_server, log_type, year, month, day, field );
+    if ( std::get<0>(result) == true ) {
+        // get data
+        std::vector<std::tuple<QString, int>> aux_items = std::get<1>(result);
+        std::vector<std::tuple<QString, int>> items;
+        items.reserve( 32 );
+
+        // cut off exdceeding elements for the chart
+        const int max_items=15;
+        int oth_count=0;
+        for ( int i=0; i<aux_items.size(); i++ ) {
+            if ( i >= max_items ) {
+                oth_count += std::get<1>( aux_items.at(i) );
+            } else {
+                items.push_back( aux_items.at( i ) );
+            }
+        }
+        aux_items.clear();
+
+        // draw the chart
+        QPieSeries *pie = new QPieSeries();
+        for ( const auto& item : items ) {
+            pie->append( std::get<0>(item), std::get<1>(item) );
+        }
+
+        if ( oth_count > 0 ) {
+            pie->append( "Others", oth_count );
+            QPieSlice *slice = pie->slices().at( pie->count()-1 );
+            slice->setBrush( Qt::gray );
+        }
+        pie->setLabelsVisible();
+
+        QChart *p_chart = new QChart();
+        p_chart->addSeries( pie );
+        p_chart->setTitle( field );
+        p_chart->setTitleFont( fonts.at( "main_big" ) );
+        p_chart->legend()->hide();
+        //p_chart->setTheme( QChart::ChartTheme::ChartThemeDark );
+
+        chart->setChart( p_chart );
+        chart->setRenderHint( QPainter::Antialiasing );
+    }
+}
+
+
+
+void Crapview::drawDay(QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts, const QString& web_server, const QString& log_type, const QString& from_year, const QString& from_month, const QString& from_day, const QString& to_year, const QString& to_month, const QString& to_day, const QString& field , const QString& filter )
+{
+    auto result = this->dbQuery.getDaytimeCounts(
+        web_server, log_type,
+        from_year, from_month, from_day,
+        to_year, to_month, to_day,
+        field, filter );
+    if ( std::get<0>(result) == true ) {
+        // get data
+        // { hour : { 10th_minutes : count } }
+        std::unordered_map<int, std::unordered_map<int, int>> aux_items = std::get<1>(result);
+
+        // draw the chart
+    }
+}
+
+
+
+void Crapview::drawRelat(QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts, const QString& web_server, const QString& log_type, const QString& from_year, const QString& from_month, const QString& from_day, const QString& to_year, const QString& to_month, const QString& to_day, const QString& field_1, const QString& filter_1, const QString& field_2, const QString& filter_2 )
+{
+
 }
