40ead9b2015c5858c10c1f484b945427a9bbe188
elB4RTO <elbarto@disroot.org>
2022-07-24 21:03:03 +0200

    Improvements
    Moved charts' titles and legends as translatable variables.
    Code improvements.

--- craplog/tools/crapview/crapview.cpp
+++ craplog/tools/crapview/crapview.cpp
@@ -117,7 +117,7 @@ void Crapview::drawCount(QtCharts::QChartView* chart, const std::unordered_map<s
         }
 
         if ( oth_count > 0 ) {
-            pie->append( "Others", oth_count );
+            pie->append( this->TEXT_COUNT_OTHERS, oth_count );
             QPieSlice *slice = pie->slices().at( pie->count()-1 );
             slice->setBrush( Qt::gray );
         }
@@ -160,7 +160,7 @@ void Crapview::drawDay(QtCharts::QChartView* chart, const std::unordered_map<std
         b_10->setColor( col );
         QBarSet *b_20 = new QBarSet( "" );
         b_20->setColor( col );
-        QBarSet *b_30 = new QBarSet( " 10 minutes gap per hour" );
+        QBarSet *b_30 = new QBarSet( this->LEGEND_DAY );
         b_30->setColor( col );
         QBarSet *b_40 = new QBarSet( "" );
         b_40->setColor( col );
@@ -169,78 +169,83 @@ void Crapview::drawDay(QtCharts::QChartView* chart, const std::unordered_map<std
         QBarSet *b_60 = new QBarSet( "" );
         b_60->setColor( col );
 
+        // build the bars upon data
         int count, max_count=0;
         for ( int h=0; h<24; h++ ) {
             auto& data = items.at( h );
-            count = data.at( 10 );
+            count = data.at( 0 );
             *b_10 << count;
             if ( count > max_count ) {
                 max_count = count;
             }
-            count = data.at( 20 );
+            count = data.at( 10 );
             *b_20 << count;
             if ( count > max_count ) {
                 max_count = count;
             }
-            count = data.at( 30 );
+            count = data.at( 20 );
             *b_30 << count;
             if ( count > max_count ) {
                 max_count = count;
             }
-            count = data.at( 40 );
+            count = data.at( 30 );
             *b_40 << count;
             if ( count > max_count ) {
                 max_count = count;
             }
-            count = data.at( 50 );
+            count = data.at( 40 );
             *b_50 << count;
             if ( count > max_count ) {
                 max_count = count;
             }
-            count = data.at( 60 );
+            count = data.at( 50 );
             *b_60 << count;
             if ( count > max_count ) {
                 max_count = count;
             }
         }
 
+        // apply the bars to the bars serie
         QBarSeries *bars = new QBarSeries();
         bars->append( b_10 ); bars->append( b_20 ); bars->append( b_30 );
         bars->append( b_40 ); bars->append( b_50 ); bars->append( b_60 );
         bars->setBarWidth( 1 );
 
-        QChart *t_chart = new QChart();
-        t_chart->addSeries( bars );
-        t_chart->setTitle( QString("Time of Day Count: %1").arg( field ) );
-        t_chart->setTitleFont( fonts.at("main") );
-        t_chart->legend()->setFont( fonts.at("main_small") );
-        t_chart->legend()->setVisible( false );
-        t_chart->setAnimationOptions( QChart::SeriesAnimations );
+        // set-up the bars chart
+        QChart *b_chart = new QChart();
+        b_chart->addSeries( bars );
+        b_chart->setTitle( QString("%1: %2").arg( this->TITLE_DAY ).arg( field ) );
+        b_chart->setTitleFont( fonts.at("main") );
+        b_chart->legend()->setFont( fonts.at("main_small") );
+        //b_chart->legend()->setVisible( true );
+        b_chart->legend()->setAlignment( Qt::AlignBottom );
+        b_chart->setAnimationOptions( QChart::SeriesAnimations );
+        //b_chart->setTheme( QChart::ChartTheme::ChartThemeBrownSand );
         //t_chart->setBackgroundBrush( Qt::darkGray );
 
+        // craft the X-axis labels
         QStringList categories;
         categories << "00" << "01" << "02" << "03" << "04" << "05" << "06" << "07" << "08" << "09" << "10" << "11"
                    << "12" << "13" << "14" << "15" << "16" << "17" << "18" << "19" << "20" << "21" << "22" << "23";
 
+        // set-up the time-of-day axis (X)
         QBarCategoryAxis *axisX = new QBarCategoryAxis();
         axisX->append( categories );
         axisX->setLabelsFont( fonts.at( "main_small" ) );
-        t_chart->addAxis( axisX, Qt::AlignBottom );
+        b_chart->addAxis( axisX, Qt::AlignBottom );
         bars->attachAxis( axisX );
 
+        // set-up the count values axis (Y)
         QValueAxis *axisY = new QValueAxis();
         axisY->setLabelFormat( "%d" );
         axisY->setTickCount( ( max_count < 16 ) ? max_count : 16 );
         axisY->setRange( 0, max_count );
         axisY->setLabelsFont( fonts.at( "main_small" ) );
-        t_chart->addAxis( axisY, Qt::AlignLeft );
+        b_chart->addAxis( axisY, Qt::AlignLeft );
         bars->attachAxis( axisY) ;
 
-        t_chart->legend()->setVisible( true );
-        t_chart->legend()->setAlignment( Qt::AlignBottom );
-        //t_chart->setTheme( QChart::ChartTheme::ChartThemeBrownSand );
-
-        chart->setChart( t_chart );
+        // apply the chart to the view
+        chart->setChart( b_chart );
         chart->setRenderHint( QPainter::Antialiasing );
 
         items.clear();
@@ -252,19 +257,19 @@ void Crapview::drawDay(QtCharts::QChartView* chart, const std::unordered_map<std
 void Crapview::drawRelat(QtCharts::QChartView* chart, const std::unordered_map<std::string, QFont>& fonts, const QString& web_server, const QString& log_type, const QString& from_year, const QString& from_month, const QString& from_day, const QString& to_year, const QString& to_month, const QString& to_day, const QString& field_1, const QString& filter_1, const QString& field_2, const QString& filter_2 )
 {
     bool period = true;
-    std::tuple<bool, std::vector<int>> result;
+    std::tuple<bool, std::vector<std::tuple<long long, int>>> result;
     if ( from_year == to_year
       && from_month == to_month
       && from_day == to_day ) {
         period = false;
-        this->dbQuery.getRelativeCountsDay(
+        this->dbQuery.getRelationalCountsDay(
             result,
             web_server, log_type,
             from_year, from_month, from_day,
             field_1, filter_1,
             field_2, filter_2 );
     } else {
-        this->dbQuery.getRelativeCountsPeriod(
+        this->dbQuery.getRelationalCountsPeriod(
             result,
             web_server, log_type,
             from_year, from_month, from_day,
@@ -276,75 +281,90 @@ void Crapview::drawRelat(QtCharts::QChartView* chart, const std::unordered_map<s
     if ( std::get<0>(result) == true ) {
         // get data
         // { hour : { 10th_minutes : count } }
-        std::vector<int> &items = std::get<1>(result);
+        std::vector<std::tuple<long long, int>> &items = std::get<1>(result);
 
         // draw the relational chart
         QLineSeries *line = new QLineSeries();
-
-        int max_y=0, x=0;
-        for ( const int& count : items ) {
-            *line << QPointF(x, count);
-            if ( count > max_y ) {
-                max_y = count;
+        // build the line upon data
+        int count, max_count=0;
+        long long time; // xD
+        for ( const auto& item : items ) {
+            time  = std::get<0>(item);
+            count = std::get<1>(item);
+            line->append( time, (double)count );
+            //*line << QPoint( time, (double)count );
+            if ( count > max_count ) {
+                max_count = count;
             }
-            x++;
         }
 
-        QColor col( 255, 127, 127 );
+        // build the area
         QAreaSeries *area  = new QAreaSeries( line );
         if ( period == false ) {
-            area->setName(QString("%1-%2-%3").arg( from_year ).arg( this->Months_s2i.value(from_month) ).arg( from_day ));
+            area->setName(QString("%1-%2-%3")
+                .arg( from_year ).arg( this->Months_s2i.value(from_month) ).arg( from_day ));
         } else {
-            area->setName(QString("from %1-%2-%3 to %4-%5-%6").arg( from_year ).arg( this->Months_s2i.value(from_month) ).arg( from_day ).arg( to_year ).arg( this->Months_s2i.value(to_month) ).arg( to_day ));
+            area->setName(QString("%1 %2-%3-%4 %5 %6-%7-%8")
+                .arg( this->LEGEND_RELAT_FROM )
+                .arg( from_year ).arg( this->Months_s2i.value(from_month) ).arg( from_day )
+                .arg( this->LEGEND_RELAT_TO )
+                .arg( to_year ).arg( this->Months_s2i.value(to_month) ).arg( to_day ));
         }
-        //area->setColor( QColor( 255, 127, 127 ) );
+
+        // color the area
+        QColor col1 = Qt::GlobalColor::red,
+               col2 = Qt::GlobalColor::yellow;
+        //area->setColor( col );
         QLinearGradient gradient(QPointF(0, 0), QPointF(0, 1));
-        gradient.setColorAt(0.0, col.lighter( 50 ) );
-        gradient.setColorAt(1.0, col.lighter( 140 ));
+        gradient.setColorAt(0.1, col1.lighter( 80 ) );
+        gradient.setColorAt(1.0, col2.lighter( 90 ) );
         gradient.setCoordinateMode(QGradient::ObjectBoundingMode);
         area->setBrush( gradient );
-        area->setBorderColor( col.lighter( 50 ) );
+        area->setBorderColor( col1.lighter( 50 ) );
 
-        // fictitious area
+        // fictitious area, just to have another label
         QAreaSeries *area_ = new QAreaSeries( );
         area_->setBrush( gradient );
-        area_->setBorderColor( col.lighter( 50 ) );
+        area_->setBorderColor( col1.lighter( 50 ) );
 
+        // build the area
         QChart *a_chart = new QChart();
         a_chart->addSeries( area );
         a_chart->addSeries( area_ );
-        a_chart->setTitle("Relational Count: "+field_1+" -> "+field_2);
+        a_chart->setTitle( QString("%1: %2 -> %3").arg(this->TITLE_RELAT).arg(field_1).arg(field_2) );
         a_chart->setTitleFont( fonts.at("main") );
         a_chart->legend()->setFont( fonts.at( "main_small" ) );
         a_chart->legend()->setAlignment( Qt::AlignBottom );
 
-        QStringList x_labels;
-        if ( period == false ) {
-            x_labels << "00" << "01" << "02" << "03" << "04" << "05" << "06" << "07" << "08" << "09" << "10" << "11"
-                     << "12" << "13" << "14" << "15" << "16" << "17" << "18" << "19" << "20" << "21" << "22" << "23";
-        } else {
-            for ( int i=0; i<items.size(); i++ ) {
-                x_labels << QString::fromStdString( std::to_string(i) );
-            }
-        }
-
-        QBarCategoryAxis *axisX = new QBarCategoryAxis();
-        axisX->append( x_labels );
+        // set-up the date-time axis (X)
+        QDateTimeAxis *axisX = new QDateTimeAxis();
         axisX->setLabelsFont( fonts.at( "main_small" ) );
         if ( period == true ) {
-            axisX->setLabelsVisible( false );
+            //axisX->setLabelsVisible( false );
+            axisX->setFormat( "yyyy-MM" );
+            int ticks = this->dbQuery.getMonthsCount( from_year, from_month, to_year, to_month );
+            if ( ticks == 1 ) {
+                axisX->setFormat( "yyyy-MM-dd" );
+                ticks = to_day.toInt() - from_day.toInt() +2;
+            }
+            axisX->setTickCount( ticks+1 );
+        } else {
+            axisX->setFormat( "hh:mm" );
+            axisX->setTickCount( 25 );
         }
         a_chart->addAxis( axisX, Qt::AlignBottom );
         area->attachAxis( axisX );
 
+        // set-up the count values axis (Y)
         QValueAxis *axisY = new QValueAxis();
         axisY->setLabelFormat( "%d" );
-        axisY->setTickCount( ( max_y < 16 ) ? max_y : 16 );
-        axisY->setRange( 0, max_y );
+        axisY->setTickCount( ( max_count < 16 ) ? max_count : 16 );
+        axisY->setRange( 0, max_count );
         axisY->setLabelsFont( fonts.at( "main_small" ) );
         a_chart->addAxis( axisY, Qt::AlignLeft );
         area->attachAxis( axisY) ;
 
+        // add the chart to the view
         chart->setChart( a_chart );
         chart->setRenderHint(QPainter::Antialiasing);
 
