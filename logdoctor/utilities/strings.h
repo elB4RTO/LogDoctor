60c57b7761b4a6cfef76fb1dcd5921b2a4f167e6
elB4RTO <elbarto@disroot.org>
2022-07-28 22:34:54 +0200

    Code improvements

--- logdoctor/utilities/strings.h
+++ logdoctor/utilities/strings.h
@@ -18,18 +18,18 @@ public:
     static bool isNumeric( const char chr );
 
     static bool startsWith( const std::string& str, const std::string& flag );
-    static bool endsWith( const std::string& str, const std::string& flag );
+    static bool endsWith(   const std::string& str, const std::string& flag );
 
     static bool contains( const std::string& str, const std::string& flag );
 
-    static std::string strip( const std::string& str, const std::string& chars=" \n\t\b\r\v" );
+    static std::string strip(  const std::string& str, const std::string& chars=" \n\t\b\r\v" );
     static std::string lstrip( const std::string& str, const std::string& chars=" \n\t\b\r\v" );
     static std::string rstrip( const std::string& str, const std::string& chars=" \n\t\b\r\v" );
     static std::string lstripUntil( const std::string& str, const std::string& chr, const bool inclusive=true, const bool consecutives=true );
 
-    static void split( std::vector<std::string>& list, const std::string& str, const std::string& sep="\n" );
+    static void split( std::vector<std::string>& list, const std::string& target_str, const std::string& separator="\n" );
 
-    static void splitrip( std::vector<std::string>& list, const std::string& str, const std::string& sep="\n", const std::string& chars=" \n\t\b\r\v" );
+    static void splitrip( std::vector<std::string>& list, const std::string& target_str, const std::string& separator="\n", const std::string& strip=" \n\t\b\r\v" );
 };
 
 #endif // STRINGS_H
