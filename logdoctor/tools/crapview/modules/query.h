7f60bf182cc8bd409e2ea4834f58b5de8a2bb96e
Valentino Orlandi <valentinox@disroot.org>
2022-08-07 23:38:13 +0200

    Removed ErrorLogs support + code improvements

--- logdoctor/tools/crapview/modules/query.h
+++ logdoctor/tools/crapview/modules/query.h
@@ -10,17 +10,13 @@
 
 
 const QHash<int, QString>
-    TYPES = {
-        {1,QMessageBox::tr("Access")}, {2,QMessageBox::tr("Error")} },
     FIELDS = {
         {0, QMessageBox::tr("Warning")},
         {10,QMessageBox::tr("Protocol")},   {11,QMessageBox::tr("Method")},
         {12,QMessageBox::tr("URI")},        {13,QMessageBox::tr("Query")},      {14,QMessageBox::tr("Response code")},
         {15,QMessageBox::tr("Time taken")}, {16,QMessageBox::tr("Bytes sent")}, {17,QMessageBox::tr("Bytes received")},
         {18,QMessageBox::tr("Referrer")},   {22,QMessageBox::tr("Cookie")},
-        {20,QMessageBox::tr("Client")},     {21,QMessageBox::tr("User-agent")},
-        {30,QMessageBox::tr("Port")},
-        {31,QMessageBox::tr("Level")}, {32,QMessageBox::tr("Message")}, {33,QMessageBox::tr("Source file")} },
+        {20,QMessageBox::tr("Client")},     {21,QMessageBox::tr("User-agent")} },
     MONTHS = {
         {1, QMessageBox::tr("January")},  {2, QMessageBox::tr("February")},  {3, QMessageBox::tr("March")},
         {4, QMessageBox::tr("April")},    {5, QMessageBox::tr("May")},       {6, QMessageBox::tr("June")},
@@ -33,22 +29,22 @@ class DbQuery
 public:
     DbQuery();
 
-    void setDialogLevel( const int new_level );
+    void setDialogLevel( const int& new_level );
 
     void setDbPath( const std::string& path );
     //const std::string getDbPath( const int web_server );
 
     const int getMonthsCount( const QString& from_year, const QString& from_month, const QString& to_year, const QString& to_month );
 
-    void refreshDates( std::tuple<bool, std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::vector<int>>>>>>& result );
+    void refreshDates( std::tuple<bool, std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, std::vector<int>>>>>& result );
 
     void updateWarnings(
-        const QString& web_server, const QString& log_type,
+        const QString& web_server,
         const std::vector<std::tuple<int, int>>& updates );
 
     void getWarnCounts(
         std::tuple<bool, std::vector<std::vector<std::vector<std::vector<QString>>>>>& result,
-        const QString& web_server, const QString& log_type,
+        const QString& web_server,
         const QString& year_,      const QString& month_,   const QString& day_, const QString& hour_ );
 
     void getSpeedData(
@@ -59,27 +55,27 @@ public:
 
     void getItemsCount(
         std::tuple<bool, std::vector<std::tuple<QString, int>>>& result,
-        const QString& web_server, const QString& log_type,
+        const QString& web_server,
         const QString& year,       const QString& month,    const QString& day,
         const QString& log_field );
 
     void getDaytimeCounts(
         std::tuple<bool, std::unordered_map<int, std::unordered_map<int, int>>>& result,
-        const QString& web_server, const QString& log_type,
+        const QString& web_server,
         const QString& from_year_, const QString& from_month_, const QString& from_day_,
         const QString& to_year_,   const QString& to_month_,   const QString& to_day_,
         const QString& log_field_, const QString& field_filter );
 
     void getRelationalCountsDay(
         std::tuple<bool, std::vector<std::tuple<long long, int>>>& result,
-        const QString& web_server,   const QString& log_type,
+        const QString& web_server,
         const QString& year_,        const QString& month_,         const QString& day_,
         const QString& log_field_1_, const QString& field_filter_1,
         const QString& log_field_2_, const QString& field_filter_2 );
 
     void getRelationalCountsPeriod(
         std::tuple<bool, std::vector<std::tuple<long long, int>>>& result,
-        const QString& web_server,   const QString& log_type,
+        const QString& web_server,
         const QString& from_year_,   const QString& from_month_,    const QString& from_day_,
         const QString& to_year_,     const QString& to_month_,      const QString& to_day_,
         const QString& log_field_1_, const QString& field_filter_1,
@@ -88,8 +84,7 @@ public:
 private:
     QString MSG_ERR_PROCESSING = QMessageBox::tr("An error occured while processing"),
             MSG_ERR_PROCESSING_DATES = QMessageBox::tr("An error occured while processing dates"),
-            MSG_ERR_UNX_WS = QMessageBox::tr("Unexpected WebServer"),
-            MSG_ERR_UNX_LT = QMessageBox::tr("Unexpected LogType");
+            MSG_ERR_UNX_WS = QMessageBox::tr("Unexpected WebServer");
 
     // quantity of informational dialogs to display
     int dialog_level = 2; // 0: essential, 1: usefull, 2: explanatory
@@ -111,11 +106,7 @@ private:
             {FIELDS.value(18), "referrer"},
             {FIELDS.value(20), "client"},
             {FIELDS.value(21), "user_agent"},
-            {FIELDS.value(22), "cookie"},
-            {FIELDS.value(30), "port"},
-            {FIELDS.value(31), "level"},
-            {FIELDS.value(32), "message"},
-            {FIELDS.value(33), "source_file"}
+            {FIELDS.value(22), "cookie"}
         };
 
     const QHash<QString, int>
@@ -125,9 +116,9 @@ private:
             {MONTHS.value(7),7},   {MONTHS.value(8),8},   {MONTHS.value(9),9},
             {MONTHS.value(10),10}, {MONTHS.value(11),11}, {MONTHS.value(12),12} };
 
-    const int getMinuteGap( const int minute, const int gap=10 ),
-              getMonthDays( const int year, const int month ),
-              getMonthsCount( const int from_year, const int from_month, const int to_year, const int to_month );
+    const int getMinuteGap( const int& minute, const int& gap=10 ),
+              getMonthDays( const int& year, const int& month ),
+              getMonthsCount( const int& from_year, const int& from_month, const int& to_year, const int& to_month );
 };
 
 #endif // QUERY_H
