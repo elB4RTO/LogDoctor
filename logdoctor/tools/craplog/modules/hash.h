341929c02ce79c5cbdc3cd7cf9c3517efc57a035
elB4RTO <elbarto@disroot.org>
2022-08-07 23:35:30 +0200

    Removed ErrorLogs support + code improvements

--- logdoctor/tools/craplog/modules/hash.h
+++ logdoctor/tools/craplog/modules/hash.h
@@ -16,7 +16,7 @@ class HashOps
 public:
     HashOps();
 
-    void setDialogLevel( const int new_level );
+    void setDialogLevel( const int& new_level );
 
     // retrieve the lists from the database file
     bool loadUsedHashesLists( const std::string& db_path );
@@ -24,10 +24,10 @@ public:
     // returns the hash
     std::string digestFile( const std::string& file_path );
     // check if the given hash is from a file which has been used already
-    bool hasBeenUsed( const std::string& file_hash, const int web_server_id, const int log_type );
+    bool hasBeenUsed( const std::string& file_hash, const int& web_server_id );
     // insert the given hash/es in the relative list
-    bool insertUsedHash( QSqlDatabase& db, QSqlQuery& query, const QString& db_name, const std::string& hash, const int web_server_id, const int log_type );
-    bool insertUsedHashes( const std::string& db_path, const std::unordered_map<int, std::vector<std::string>>& hashes, const int web_server_id );
+    bool insertUsedHash( QSqlDatabase& db, QSqlQuery& query, const QString& db_name, const std::string& hash, const int& web_server_id );
+    bool insertUsedHashes( const std::string& db_path, const std::vector<std::string>& hashes, const int& web_server_id );
 
 private:
     int dialog_level = 2;
@@ -36,16 +36,18 @@ private:
     const int APACHE_ID=11, NGINX_ID=12, IIS_ID=13;
 
     const std::unordered_map<int, QString> ws_names = {
-        {11, "apache"},
-        {12, "nginx"},
-        {13, "iis"} };
-    const std::unordered_map<int, QString> log_types = {
-        {1, "access"},
-        {2, "error"} };
+        {this->APACHE_ID, "apache"},
+        {this->NGINX_ID,  "nginx"},
+        {this->IIS_ID,    "iis"} };
 
 
     // lists of used files' hashes
-    std::unordered_map<int, std::unordered_map<int, std::vector<std::string>>> hashes;
+    // { web_server_id : { hashes } }
+    std::unordered_map<int, std::vector<std::string>> hashes = {
+        {this->APACHE_ID, {}},
+        {this->NGINX_ID,  {}},
+        {this->IIS_ID,    {}}
+    };
 
 };
 
