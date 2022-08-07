f4946aa25f7d526d1624ef7aac0775f0f1d38b28
Valentino Orlandi <valentinox@disroot.org>
2022-08-07 23:32:28 +0200

    Added new custom exceptions classes
    Added 'GenericException', 'WebServerException', 'LogParserException' and
    'DateTimeException'

--- logdoctor/modules/exceptions.cpp
+++ logdoctor/modules/exceptions.cpp
@@ -2,12 +2,61 @@
 #include "exceptions.h"
 
 
+/////////////////
+//// GENERIC ////
+GenericException::GenericException(const std::string& msg )
+{
+    this->msg = msg;
+}
+const std::string& GenericException::what()
+{
+    return msg;
+}
+
+
+////////////////////
+//// WEB SERVER ////
+WebServerException::WebServerException(const std::string& msg )
+{
+    this->msg = msg;
+}
+const std::string& WebServerException::what()
+{
+    return msg;
+}
+
+
+////////////////////
+//// LOG FORMAT ////
 LogFormatException::LogFormatException(const std::string& msg )
 {
     this->msg = msg;
 }
-
 const std::string& LogFormatException::what()
+{
+    return msg;
+}
+
+
+////////////////////
+//// LOG PARSER ////
+LogParserException::LogParserException(const std::string& msg )
+{
+    this->msg = msg;
+}
+const std::string& LogParserException::what()
+{
+    return msg;
+}
+
+
+///////////////////
+//// DATE-TIME ////
+DateTimeException::DateTimeException(const std::string& msg )
+{
+    this->msg = msg;
+}
+const std::string& DateTimeException::what()
 {
     return msg;
 }
--- logdoctor/modules/exceptions.h
+++ logdoctor/modules/exceptions.h
@@ -5,9 +5,49 @@
 #include <exception>
 
 
+class GenericException : public std::exception {
+public:
+    GenericException( const std::string& msg );
+    const std::string& what();
+
+private:
+    std::string msg;
+};
+
+
+class WebServerException : public std::exception {
+public:
+    WebServerException( const std::string& msg );
+    const std::string& what();
+
+private:
+    std::string msg;
+};
+
+
 class LogFormatException : public std::exception {
 public:
     LogFormatException( const std::string& msg );
+    const std::string& what();
+
+private:
+    std::string msg;
+};
+
+
+class LogParserException : public std::exception {
+public:
+    LogParserException( const std::string& msg );
+    const std::string& what();
+
+private:
+    std::string msg;
+};
+
+
+class DateTimeException : public std::exception {
+public:
+    DateTimeException( const std::string& msg );
     const std::string& what();
 
 private:
