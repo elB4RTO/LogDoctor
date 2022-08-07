81a740d0ac2bc82fed0b42c4e1d037e3cdc195c5
elB4RTO <elbarto@disroot.org>
2022-08-07 23:34:21 +0200

    Removed ErrorLogs support + code improvements

--- logdoctor/tools/craplog/craplog.h
+++ logdoctor/tools/craplog/craplog.h
@@ -21,10 +21,8 @@ public:
 
     void run();
 
-    const int getDialogsLevel();
-    void setDialogLevel( const int new_level );
-
-    void setChartsTheme( const int new_theme_id );
+    const int& getDialogsLevel();
+    void setDialogLevel( const int& new_level );
 
     const std::string
         & getStatsDatabasePath(),
@@ -33,26 +31,20 @@ public:
          setHashesDatabasePath( const std::string& path );
 
     // logs formats web server specific settings
-    void setApacheALF( const std::string& format_string );
-    void setNginxALF(  const std::string& format_string );
-    void setIisALF(    const std::string& format_string, const int log_module );
-    void setApacheELF( const std::string& format_string );
-    void setNginxELF(  const std::string& format_string );
-    //void setIisELF(    const std::string& format_string );
-    const std::string& getAccessLogsFormatString( const int web_server_id );
-    const std::string& getErrorLogsFormatString( const int web_server_id );
-    const FormatOps::LogsFormat& getAccessLogsFormat( const int web_server_id );
-    const FormatOps::LogsFormat& getErrorLogsFormat( const int web_server_id );
-    const QString getLogsFormatSample( const int web_server_id, const int log_type );
+    void setApacheLogFormat( const std::string& format_string );
+    void setNginxLogFormat(  const std::string& format_string );
+    void setIisLogFormat(    const std::string& format_string, const int& log_module );
+    const std::string& getLogsFormatString( const int& web_server_id );
+    const FormatOps::LogsFormat& getLogsFormat( const int& web_server_id );
+    const QString getLogsFormatSample( const int& web_server_id );
     // currently used WS and LF
-    void setCurrentWSID( const int web_server_id );
-    const int getCurrentWSID();
-    const FormatOps::LogsFormat& getCurrentALF();
-    const FormatOps::LogsFormat& getCurrentELF();
+    void setCurrentWSID( const int& web_server_id );
+    const int& getCurrentWSID();
+    const FormatOps::LogsFormat& getCurrentLogFormat();
 
     // logs paths
-    const std::string& getLogsPath( const int web_server, const int log_type );
-    void setLogsPath( const int web_server, const int log_type, const std::string& new_path );
+    const std::string& getLogsPath( const int& web_server );
+    void setLogsPath( const int& web_server, const std::string& new_path );
 
     // log file item's infoes
     struct LogFile {
@@ -62,10 +54,9 @@ public:
         QString name;
         std::string hash;
         std::string path;
-        LogOps::LogType type;
     };
     // logs list related methods
-    const std::vector<LogFile>& getLogsList( const bool fresh=false );
+    const std::vector<LogFile>& getLogsList( const bool& fresh=false );
     const int getLogsListSize();
     // log file related
     const LogFile& getLogFileItem( const QString& file_name );
@@ -73,11 +64,11 @@ public:
     // set a file in the list as selected, to be used for the stats
     const bool setLogFileSelected( const QString& file_name );
     // check if a file name respects the one set
-    const bool isFileNameValid( const std::string& name, const LogOps::LogType& log_type );
+    const bool isFileNameValid( const std::string& name );
 
     // warning file size (in Bytes)
-    const int getWarningSize();
-    void setWarningSize( const int new_size );
+    const long& getWarningSize();
+    void setWarningSize( const long int& new_size );
     // logs usage control
     HashOps hashOps;
 
@@ -89,41 +80,41 @@ public:
         bool used;
         std::vector<std::string> list;
     };
-    const bool isBlacklistUsed( const int web_server_id, const int log_type, const int log_field_id ),
-               isWarnlistUsed( const int web_server_id, const int log_type, const int log_field_id );
-    void setBlacklistUsed( const int web_server_id, const int log_type, const int log_field_id, const bool used ),
-         setWarnlistUsed( const int web_server_id, const int log_type, const int log_field_id, const bool used );
+    const bool& isBlacklistUsed( const int& web_server_id, const int& log_field_id ),
+              & isWarnlistUsed( const int& web_server_id, const int& log_field_id );
+    void setBlacklistUsed( const int& web_server_id, const int& log_field_id, const bool& used ),
+         setWarnlistUsed( const int& web_server_id, const int& log_field_id, const bool& used );
     const std::vector<std::string>
-        &getBlacklist( const int web_server_id, const int log_type, const int log_field_id ),
-        &getWarnlist( const int web_server_id, const int log_type, const int log_field_id );
-    void blacklistAdd( const int web_server_id, const int log_type, const int log_field_id, const std::string& new_item ),
-         warnlistAdd( const int web_server_id, const int log_type, const int log_field_id, const std::string& new_item ),
-         blacklistRemove( const int web_server_id, const int log_type, const int log_field_id, const std::string& new_item ),
-         warnlistRemove( const int web_server_id, const int log_type, const int log_field_id, const std::string& item );
-    int blacklistMoveUp( const int web_server_id, const int log_type, const int log_field_id, const std::string& item ),
-        warnlistMoveUp( const int web_server_id, const int log_type, const int log_field_id, const std::string& item ),
-        blacklistMoveDown( const int web_server_id, const int log_type, const int log_field_id, const std::string& item ),
-        warnlistMoveDown( const int web_server_id, const int log_type, const int log_field_id, const std::string& item );
+        &getBlacklist( const int& web_server_id, const int& log_field_id ),
+        &getWarnlist( const int& web_server_id, const int& log_field_id );
+    void blacklistAdd( const int& web_server_id, const int& log_field_id, const std::string& new_item ),
+         warnlistAdd( const int& web_server_id, const int& log_field_id, const std::string& new_item ),
+         blacklistRemove( const int& web_server_id, const int& log_field_id, const std::string& new_item ),
+         warnlistRemove( const int& web_server_id, const int& log_field_id, const std::string& item );
+    int blacklistMoveUp( const int& web_server_id, const int& log_field_id, const std::string& item ),
+        warnlistMoveUp( const int& web_server_id, const int& log_field_id, const std::string& item ),
+        blacklistMoveDown( const int& web_server_id, const int& log_field_id, const std::string& item ),
+        warnlistMoveDown( const int& web_server_id, const int& log_field_id, const std::string& item );
 
     // job related
     const bool checkStuff();
     void startWorking(),
          stopWorking(),
-         makeCharts( const QChart::ChartTheme& theme, const std::unordered_map<std::string, QFont>& fonts, QChartView* acc_chart, QChartView* err_chart, QChartView* traf_chart ),
+         makeCharts( const QChart::ChartTheme& theme, const std::unordered_map<std::string, QFont>& fonts, QChartView* size_chart, QChartView* traf_chart ),
          clearDataCollection();
-    const bool isWorking(),
-               isParsing();
+    const bool& isWorking(),
+              & isParsing();
     // job performancea
     void collectPerfData(),
-         sumBlacklistededSize( const int size, const int log_type ),
-         sumPerfSize( const int size, const int log_type );
-    const int getPerfSize(),
-              getTotalSize(),
-              getParsedSize(),
-              getParsedLines();
-              //getAccessSize(),
-              //getErrorSize(),
-              //getBlacklistedSize( const int log_type );
+         sumBlacklistededSize( const int& size ),
+         sumPerfSize( const int& size );
+    const int& getPerfSize(),
+             & getTotalSize(),
+             & getParsedSize(),
+             & getParsedLines();
+             /*& getAccessSize(),
+             & getErrorSize(),
+             & getBlacklistedSize( const int& log_type );*/
 
 private:
     // quantity of informational dialogs to display
@@ -131,8 +122,6 @@ private:
 
     // web servers ID constants
     const unsigned int APACHE_ID=11, NGINX_ID=12, IIS_ID=13;
-    // log type constants
-    const unsigned int FAILED=0, ACCESS_LOGS=1, ERROR_LOGS=2;
 
     // databases paths
     std::string db_stats_path,
@@ -143,66 +132,58 @@ private:
          parsing = false,
          proceed = false;
     // perf related
-    int perf_size = 0,
-        total_size = 0,
-        parsed_size = 0,
-        total_lines  = 0,
+    int total_lines  = 0,
         parsed_lines = 0,
-
-        total_access_size = 0,
-        total_error_size  = 0,
-        parsed_access_size = 0,
-        parsed_error_size  = 0,
-        blacklisted_access_size = 0,
-        blacklisted_error_size  = 0;
+        perf_size        = 0,
+        total_size       = 0,
+        parsed_size      = 0,
+        blacklisted_size = 0;
     // chart related
-    const QString printableSize( const int bytes );
-    const std::vector<int> calcDayTraffic( const int log_type );
+    const QString printableSize( const int& bytes );
+    const std::vector<int> calcDayTraffic();
 
-    // data collection
+    // data collection, each item results from a log line
     /* structure
-     *      { log_type_id : [ { log_field_id : "data" } ] }
-     * log_type_ids
-     *      1: access_logs, 2: error_logs
+     *      [ { log_field_id : "data" } ]
+     *
      * log_field_ids
      *      99: warning,
      *      1: year, 2: month, 3: day, 4: hour, 5: minute, 6:second,
      *      10: request_protocol, 11: request_method, 12: request_page, 13: request_query, 14: response_code,
      *      15: time_taken, 16: bytes_sent, 17: bytes_received, 18: referrer,
-     *      20: client, 21: user_agent, 22: cookie,
-     *      30: port, 31: error_level, 32: error_message, 33: source_file
+     *      20: client, 21: user_agent, 22: cookie
     */
-    std::unordered_map<int, std::vector<std::unordered_map<int, std::string>>> data_collection;
+    std::vector<std::unordered_map<int, std::string>> data_collection;
     std::vector<LogFile> log_files_to_use;
-    std::vector<std::string> access_logs_lines, error_logs_lines;
+    std::vector<std::string> logs_lines;
     void joinLogLines(),
          parseLogLines(),
          storeLogLines();
     // used files
-    std::unordered_map<int, std::vector<std::string>> used_files_hashes;
+    std::vector<std::string> used_files_hashes;
 
     // configs location
     std::string configs_path;
 
     // control related
-    long int warning_size = (1'048'576 * 50) +1; // in Bytes ( => 1 MiB * x )
+    long warning_size = (1'048'576 * 50) +1; // in Bytes ( => 1 MiB * x )
     // black/warn-list
-    // { web_server_id : { log_type : { log_field_id : BWlist } } }
-    std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, BWlist>>>
+    // { web_server_id : { log_field_id : BWlist } }
+    std::unordered_map<int, std::unordered_map<int, BWlist>>
         blacklists,
         warnlists;
 
     // web server related
-    int current_WS = 11;
+    int current_WS = this->APACHE_ID;
     // web server specific paths
-    std::unordered_map<int, std::unordered_map<int, std::string>> logs_paths;
+    std::unordered_map<int, std::string> logs_paths;
     // web server specific file names criterions
     struct LogName {
         std::string starts;
         std::string contains;
         std::string ends;
     };
-    std::unordered_map<int, std::unordered_map<int, LogName>> logs_base_names;
+    std::unordered_map<int, LogName> logs_base_names;
 
     // logs list related
     std::vector<LogFile> logs_list;
@@ -210,12 +191,10 @@ private:
 
     // logs format related
     FormatOps formatOps;
-    std::unordered_map<int, std::unordered_map<int, std::string>> logs_format_strings;
-    std::unordered_map<int, std::unordered_map<int, FormatOps::LogsFormat>> logs_formats;
-    void setCurrentALF();
-    void setCurrentELF();
-    FormatOps::LogsFormat current_ALF;
-    FormatOps::LogsFormat current_ELF;
+    std::unordered_map<int, std::string> logs_format_strings;
+    std::unordered_map<int, FormatOps::LogsFormat> logs_formats;
+    void setCurrentLogFormat();
+    FormatOps::LogsFormat current_LF;
 
     void readConfigs();
 
