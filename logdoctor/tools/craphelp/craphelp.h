7658297596c36b089159ccaea7b3c47622d53da9
Valentino Orlandi <valentinox@disroot.org>
2022-08-11 20:55:08 +0200

    Added new tool Craphelp

---
+++ logdoctor/tools/craphelp/craphelp.h
@@ -0,0 +1,26 @@
+#ifndef CRAPHELP_H
+#define CRAPHELP_H
+
+#include <QWidget>
+
+namespace Ui {
+class Craphelp;
+}
+
+class Craphelp : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Craphelp(QWidget *parent = nullptr);
+    ~Craphelp();
+
+    void helpLogsFormat( const std::string& base_path, const std::string& web_server, const std::string& language, const QFont& font, const int& color_scheme_id );
+
+private:
+    Ui::Craphelp *ui;
+
+    const std::unordered_map<std::string, QString> getColorScheme( const int& scheme_id );
+};
+
+#endif // CRAPHELP_H
