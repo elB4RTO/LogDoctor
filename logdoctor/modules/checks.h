d0c39da0e46db3c86f602516cfe5055e30629b60
Valentino Orlandi <valentinox@disroot.org>
2022-08-22 21:17:48 +0200

    Removed unused method

--- logdoctor/modules/checks.h
+++ logdoctor/modules/checks.h
@@ -12,9 +12,6 @@ public:
     static bool checkStatsDatabase( const std::string& db_path );
     static bool checkHashesDatabase( const std::string& db_path );
 
-    static bool checkCraplog( const Craplog& craplog );
-
-
 private:
     static int checkDatabaseTablesNames( QSqlDatabase& db, const std::string& db_path, const QString& db_name );
     static bool newStatsDatabase( QSqlDatabase& db, const std::string& db_path, const QString& db_name, const std::vector<QString>& ws_names );
