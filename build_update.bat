d52bfaa081f4d9610837d0cdc55d12db80c4df49
Valentino Orlandi <valentinox@disroot.org>
2022-08-27 10:33:20 +0200

    Fixes

--- build_update.bat
+++ build_update.bat
@@ -3,14 +3,15 @@
 SET actual_path=%CD%
 
 :: Get the path of LogDoctor-git's folder and move in
-SET docdir=%~dp1
+SET docdir=%~dp0
 cd "%docdir%"
 
 :: Check the existence of a previous executable file
-IF NOT EXIST C:\ProgramFiles\LogDoctor\ {
+IF NOT EXIST C:\ProgramFiles\LogDoctor\ (
 	ECHO "Warning: no previous installation detected, please run the installation script instead"
-	EXIT /B /B
-}
+	cd "%actual_path%"
+	EXIT /B
+)
 
 :: Start the compilation process
 ECHO "Starting compilation process"
@@ -20,19 +21,21 @@ mkdir build && cd build
 
 :: Prepare the cmake files
 cmake ../logdoctor -DCMAKE_BUILD_TYPE=MinSizeRel
-IF ERRORLEVEL 1 {
+IF ERRORLEVEL 1 (
 	:: an error occured during preparation
 	ECHO "Error: failed to prepare cmake files"
-	EXIT /B /B
-}
+	cd "%actual_path%"
+	EXIT /B
+)
 
 :: Build the project
 cmake --build ./ --target all
-IF ERRORLEVEL 1 {
+IF ERRORLEVEL 1 (
 	:: an error occured during compilation
 	ECHO "Error: failed to compile"
-	EXIT /B /B
-}
+	cd "%actual_path%"
+	EXIT /B
+)
 
 :: Compilation finished
 ECHO "Compilation finished"
@@ -42,30 +45,35 @@ ECHO "Starting update process"
 cd ../installation_stuff/
 
 
-IF NOT EXIST %USERPROFILE%\AppData\Local\LogDoctor {
+IF NOT EXIST %USERPROFILE%\AppData\Local\LogDoctor (
 	mkdir %USERPROFILE%\AppData\Local\LogDoctor
-	IF ERRORLEVEL 1 {
+	IF ERRORLEVEL 1 (
 		ECHO "Error: failed to create directory: %USERPROFILE%\AppData\Local\LogDoctor"
+		cd "%actual_path%"
 		EXIT /B
-	}
-}
+	)
+)
 copy logdocdata\* %USERPROFILE%\AppData\Local\LogDoctor\
-IF ERRORLEVEL 1 {
+IF ERRORLEVEL 1 (
 	ECHO "Error: failed to copy LogDoctor's data"
-}
+	cd "%actual_path%"
+	EXIT /B
+)
 
 
 runas /user:administrator copy LogDoctor.exe C:\ProgramFiles\LogDoctor\
-IF ERRORLEVEL 1 {
+IF ERRORLEVEL 1 (
 	ECHO "Error: failed to copy the executable"
+	cd "%actual_path%"
 	EXIT /B
-}
+)
 
 runas /user:administrator copy ./logdoctor.svg C:\ProgramFiles\LogDoctor\LogDoctor.svg
-IF ERRORLEVEL 1 {
+IF ERRORLEVEL 1 (
 	ECHO "Error: failed to copy the icon"
+	cd "%actual_path%"
 	EXIT /B
-}
+)
 
 
 :: Update finished
