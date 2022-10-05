9253c5bef0ba78100f1a5df14f579df1c39ec539
elB4RTO <elbarto@disroot.org>
2022-10-05 21:21:37 +0200

    Added custom Discard-Ignore-Abort choice Dialog

---
+++ logdoctor/modules/dialogs/dialogdia.h
@@ -0,0 +1,29 @@
+#ifndef DIALOGDIA_H
+#define DIALOGDIA_H
+
+#include <QDialog>
+
+namespace Ui {
+    class DialogDia;
+}
+
+class DialogDia : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogDia( const QString& title, const QString& text, const bool& ignore=true, const bool& discard=true, const bool& abort=true, QWidget *parent=nullptr );
+    ~DialogDia();
+
+private slots:
+    void on_button_IGNORE_clicked();
+
+    void on_button_DISCARD_clicked();
+
+    void on_button_ABORT_clicked();
+
+private:
+    Ui::DialogDia *ui;
+};
+
+#endif // DIALOGDIA_H
