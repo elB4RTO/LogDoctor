bb02a154ea33b24c6f987cd6001892240aa395c3
Valentino Orlandi <valentinox@disroot.org>
2022-10-05 21:20:42 +0200

    Added custom Yes-No choice Dialog

---
+++ logdoctor/modules/dialogs/dialogbool.cpp
@@ -0,0 +1,34 @@
+#include "dialogbool.h"
+#include "ui_dialogbool.h"
+
+DialogBool::DialogBool( const QString& title, const QString& text, QWidget *parent ) :
+    QDialog(parent),
+    ui(new Ui::DialogBool)
+{
+    ui->setupUi(this);
+
+    // insert the given title and text
+    this->ui->label_Title->setText( title );
+    this->ui->label_Message->setText( text );
+
+    // adjust the initial size
+    this->adjustSize();
+}
+
+DialogBool::~DialogBool()
+{
+    delete ui;
+}
+
+
+void DialogBool::on_button_NO_clicked()
+{
+    this->done( 0 );
+}
+
+
+void DialogBool::on_button_YES_clicked()
+{
+    this->done( 1 );
+}
+
