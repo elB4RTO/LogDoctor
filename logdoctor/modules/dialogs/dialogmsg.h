e28c573a85448ce49fcfd9670ad6ef8e2ec13247
Valentino Orlandi <valentinox@disroot.org>
2022-10-05 21:19:39 +0200

    Added custom Informational Dialog

---
+++ logdoctor/modules/dialogs/dialogmsg.h
@@ -0,0 +1,32 @@
+#ifndef DIALOGMSG_H
+#define DIALOGMSG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class DialogMsg;
+}
+
+class DialogMsg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogMsg( const QString& title, const QString& text, const QString& additional, const int& type, QWidget *parent=nullptr );
+    ~DialogMsg();
+
+private slots:
+    void on_button_ShowAdditional_clicked();
+
+    void on_button_Ok_clicked();
+
+private:
+    Ui::DialogMsg *ui;
+
+    bool additional_shown = false;
+
+    int initial_height    = 0;
+    int additional_height = 0;
+};
+
+#endif // DIALOGMSG_H
