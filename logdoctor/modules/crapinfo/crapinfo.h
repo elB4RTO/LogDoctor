b65b8a03f009b3a2b0894e2abb0bad8147b9742e
Valentino Orlandi <valentinox@disroot.org>
2022-11-13 01:30:30 +0100

    Improvements and updates
    UI updates.
    Added stylesheets support.

--- logdoctor/modules/crapinfo/crapinfo.h
+++ logdoctor/modules/crapinfo/crapinfo.h
@@ -13,11 +13,13 @@ class Crapinfo : public QWidget
     Q_OBJECT
 
 public:
-    explicit Crapinfo( const QString& version, const QString& exec_path, const QString& conf_path, const QString& logdoc_path, QWidget *parent=nullptr);
+    explicit Crapinfo( const int& window_theme_id, const QString& version, const QString& exec_path, const QString& conf_path, const QString& logdoc_path, QWidget *parent=nullptr );
     ~Crapinfo();
 
 private:
     Ui::Crapinfo *ui;
+
+    void getStyleSheet( QString& stylesheet, const int& theme_id );
 };
 
 #endif // CRAPINFO_H
