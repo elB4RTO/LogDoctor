edf9f9b14561ee30d75bf135be05503047d3a94e
Valentino Orlandi <valentinox@disroot.org>
2022-11-13 01:35:16 +0100

    Improvements and updates
    Added stylesheets support.
    Added UI to display the running state and replace dialogs.
    Improved requests handling.

--- logdoctor/modules/crapup/crapup.h
+++ logdoctor/modules/crapup/crapup.h
@@ -1,18 +1,50 @@
 #ifndef CRAPUP_H
 #define CRAPUP_H
 
-#include <QObject>
+#include <QWidget>
+#include <QPixmap>
+#include <QTimer>
+#include <QNetworkReply>
 
 
-class Crapup : public QObject
+namespace Ui {
+class Crapup;
+}
+
+class Crapup : public QWidget
 {
     Q_OBJECT
 public:
-    Crapup();
+    Crapup( const int& window_theme_id, const QString& icons_theme, QWidget* parent=nullptr );
+    ~Crapup();
 
-    // perform a GET request for a version-check
-    void versionCheck( const float& current_version, const int& dialog_level );
+    void versionCheck( const float& current_version );
 
+signals:
+    void abortRequest();
+
+private slots:
+    void closeEvent( QCloseEvent* event );
+
+    void requestTimeout();
+
+    void rotateImg();
+
+private:
+    Ui::Crapup *ui;
+
+    QPixmap img_checking;
+    float img_orientation = 0.0;
+    QTimer* img_timer = nullptr;
+
+    void getStyleSheet( QString& stylesheet, const int& theme_id );
+
+    bool request_aborted;
+    QTimer* request_timer = nullptr;
+    QNetworkReply* reply = nullptr;
+    void deleteReply();
+
+    bool quitting = false;
 };
 
 #endif // CRAPUP_H
