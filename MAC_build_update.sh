6ff955d380b0d36b8810e61976ba13fb00c5d5e5
Valentino Orlandi <valentinox@disroot.org>
2022-11-16 17:03:45 +0100

    Updated MAC build scripts

--- MAC_build_update.sh
+++ MAC_build_update.sh
@@ -4,24 +4,33 @@
 actual_path=$(pwd)
 
 # Get the path of LogDoctor-git's folder and move in
-docdir="$(dirname $(realpath $0))"
+docdir="$(dirname $0)"
 cd "$docdir"
 
 # Check cmake availability
 if [[ $(which cmake) =~ ^/ ]]
-	then
-		$()
-	else
-		echo "Error: Cmake is not installed"
-		exit
-	fi
+then
+	$()
+else
+	echo "Error: Cmake not found in PATH, please add it or compilation won't socceed"
+	exit
+fi
+
+# Check Qt availability
+if [[ $(which qmake) =~ ^/ ]]
+then
+	$()
+else
+	echo "Error: Qt-clang not found in PATH, please add it or compilation won't socceed"
+	exit
+fi
 
 # Check the existence of a previous executable file
 if [ ! -e /Applications/LogDoctor.app ]
-	then
-		echo "Warning: no previous installation detected, please run the installation script instead"
-		exit
-	fi
+then
+	echo "Warning: no previous installation detected, please run the installation script instead"
+	exit
+fi
 
 # Start the compilation process
 echo "Starting compilation process"
@@ -32,25 +41,23 @@ mkdir build && cd build
 # Prepare the cmake files
 cmake ../logdoctor -DCMAKE_BUILD_TYPE=MinSizeRel
 if [[ "$?" != "0" ]]
-	then
-		# an error occured during preparation
-		echo "Error: failed to prepare cmake files"
-		exit
-	fi
+then
+	# an error occured during preparation
+	echo "Error: failed to prepare cmake files"
+	exit
+fi
 
 # Build the project
 cmake --build ./ --target all
 if [[ "$?" == "0" ]]
-	then
-		# compiled succesfully
-		echo "Done compiling"
-		chmod 755 ./LogDoctor
-		mv LogDoctor ../installation_stuff/LogDoctor.app/bin/
-	else
-		# an error occured during compilation
-		echo "Error: failed to compile"
-		exit
-	fi
+then
+	# compiled succesfully
+	chmod 755 ./LogDoctor.app/Contents/MacOS/LogDoctor
+else
+	# an error occured during compilation
+	echo "Error: failed to compile"
+	exit
+fi
 
 # Compilation finished
 wait
@@ -62,29 +69,39 @@ cd ../installation_stuff/
 
 
 if [ ! -e ~/"Lybrary/Application Support/LogDoctor" ]
+then
+	mkdir -p ~/"Lybrary/Application Support/LogDoctor"
+	if [[ "$?" != "0" ]]
 	then
-		mkdir -p ~/"Lybrary/Application Support/LogDoctor"
-		if [[ "$?" != "0" ]]
-			then
-				echo "Error: failed to create directory: '~/Lybrary/Application Support/LogDoctor'"
-				exit
-	fi
-cp -r --no-preserve=all ./logdocdata/help ~/"Lybrary/Application Support/LogDoctor"
-if [[ "$?" != "0" ]]
-	then
-		echo "Error: failed to copy LogDoctor's data"
+		echo "Error: failed to create directory: '~/Lybrary/Application Support/LogDoctor'"
 		exit
 	fi
-
-
-cd ../build
-cp --no-preserve=all ./LogDoctor.app ./LogDoctor.app.copy
-sudo mv ./LogDoctor.app.copy /Applications/LogDoctor.app
-if [[ "$?" != "0" ]]
+fi
+if [ ! -e ~/"Lybrary/Application Support/LogDoctor/help" ]
+then
+	rm -r ~/"Lybrary/Application Support/LogDoctor/help"
+	if [[ "$?" != "0" ]]
 	then
-		echo "Error: failed to copy the executable"
+		echo "Error: failed to remove old resources: ~/Lybrary/Application Support/LogDoctor/help"
 		exit
 	fi
+fi
+cp -r ./logdocdata/help ~/"Lybrary/Application Support/LogDoctor"
+if [[ "$?" != "0" ]]
+then
+	echo "Error: failed to copy LogDoctor's data"
+	exit
+fi
+
+
+cp ./osx_bundle/* ../build/LogDoctor.app/Contents/
+sudo rm -r /Applications/LogDoctor.app
+sudo mv ../build/LogDoctor.app /Applications/
+if [[ "$?" != "0" ]]
+then
+	echo "Error: failed to copy the executable"
+	exit
+fi
 
 
 # Update finished
