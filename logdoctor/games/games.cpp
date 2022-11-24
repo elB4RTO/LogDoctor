4cc294874a6e227f7aa5246a9c3e1f2337e34209
Valentino Orlandi <valentinox@disroot.org>
2022-11-24 22:29:17 +0100

    Updated games stylesheets

--- logdoctor/games/games.cpp
+++ logdoctor/games/games.cpp
@@ -147,12 +147,20 @@ void GameSec::snakeStyleSheet( QString& stylesheet, const int& theme_id )
             break;
         case 1:
             style = {
+                {"text",
+                    "rgb( 248, 248, 248 )"},
                 {"window_base_primary",
                     "rgb( 16, 16, 16 )"},
                 {"window_base_secondary",
                     "rgb( 32, 32, 32 )"},
                 {"window_border",
                     "rgb( 128, 128, 128 )"},
+                {"gamemode_box_base",
+                    "rgb( 64, 64, 64 )"},
+                {"gamemode_box_base_selection",
+                    "rgb( 96, 96, 96 )"},
+                {"gamemode_box_border",
+                    "rgb( 82, 82, 82 )"},
                 {"play_button_base",
                     "rgb( 64, 64, 64 )"},
                 {"play_button_base_hover",
@@ -173,12 +181,20 @@ void GameSec::snakeStyleSheet( QString& stylesheet, const int& theme_id )
             break;
         case 2:
             style = {
+                {"text",
+                    "rgb( 45, 0, 30 )"},
                 {"window_base_primary",
                     "rgb( 255, 140, 141 )"},
                 {"window_base_secondary",
                     "rgb( 255, 204, 143 )"},
                 {"window_border",
                     "rgb( 195, 80, 81 )"},
+                {"gamemode_box_base",
+                    "rgb( 114, 235, 115 )"},
+                {"gamemode_box_base_selection",
+                    "rgb( 154, 255, 155 )"},
+                {"gamemode_box_border",
+                    "rgb( 245, 172, 142 )"},
                 {"play_button_base",
                     "rgb( 114, 235, 115 )"},
                 {"play_button_base_hover",
@@ -199,11 +215,19 @@ void GameSec::snakeStyleSheet( QString& stylesheet, const int& theme_id )
             break;
         case 3:
             style = {
+                {"text",
+                    "rgb( 220, 211, 187 )"},
                 {"window_base_primary",
                     "rgb( 14, 28, 0 )"},
                 {"window_base_secondary",
                     "rgb( 24, 48, 0 )"},
                 {"window_border",
+                    "rgb( 163, 145, 99 )"},
+                {"gamemode_box_base",
+                    "rgb( 94, 118, 70 )"},
+                {"gamemode_box_base_selection",
+                    "rgb( 124, 148, 100 )"},
+                {"gamemode_box_border",
                     "rgb( 163, 145, 99 )"},
                 {"play_button_base",
                     "rgb( 94, 118, 70 )"},
@@ -225,12 +249,20 @@ void GameSec::snakeStyleSheet( QString& stylesheet, const int& theme_id )
             break;
         case 4:
             style = {
+                {"text",
+                    "rgb( 30, 21, 0 )"},
                 {"window_base_primary",
                     "rgb( 170, 161, 137 )"},
                 {"window_base_secondary",
                     "rgb( 230, 221, 197 )"},
                 {"window_border",
                     "rgb( 80, 71, 47 )"},
+                {"gamemode_box_base",
+                    "rgb( 130, 121, 97 )"},
+                {"gamemode_box_base_selection",
+                    "rgb( 140, 131, 107 )"},
+                {"gamemode_box_border",
+                    "rgb( 90, 81, 57 )"},
                 {"play_button_base",
                     "rgb( 130, 121, 97 )"},
                 {"play_button_base_hover",
@@ -264,9 +296,19 @@ void GameSec::snakeStyleSheet( QString& stylesheet, const int& theme_id )
             "   border: 1px solid "+style.at("window_border")+";"
             "   background-color: "+style.at("window_base_secondary")+";"
             "}"
+            "QComboBox {"
+            "   color: "+style.at("text")+";"
+            "   background-color: "+style.at("gamemode_box_base")+";"
+            "   selection-background-color: "+style.at("gamemode_box_base_selection")+";"
+            "}"
+            "QComboBox QAbstractItemView {"
+            "   border: 1px solid "+style.at("gamemode_box_border")+";"
+            "   background-color: "+style.at("gamemode_box_base")+";"
+            "}"
             "QPushButton#button_Play {"
             "   border-radius: 4px;"
             "   border: 1px solid "+style.at("play_button_border")+";"
+            "   color: "+style.at("text")+";"
             "   background-color: "+style.at("play_button_base")+";"
             "}"
             "QPushButton#button_Play:hover {"
