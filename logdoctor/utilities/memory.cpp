28af913e7279bb7b0bea8e378f1ffec6099487c2
Valentino Orlandi <valentinox@disroot.org>
2023-05-01 01:08:26 +0200

    Fix
    Fixed MemOps::availableMemory for BSD

--- logdoctor/utilities/memory.cpp
+++ logdoctor/utilities/memory.cpp
@@ -3,17 +3,17 @@
 
 #include "modules/exceptions.h"
 
-// 1: linux, 2:windows, 3:mac
-#if defined( Q_OS_DARWIN )
-    // Darwin-based systems: macOS, iOS, watchOS and tvOS.
+#if defined( Q_OS_UNIX )
     #include <unistd.h>
-    #include <mach/mach.h>
+    #if defined( Q_OS_BSD4 )
+        #include <sys/types.h>
+        #include <sys/sysctl.h>
+        #include <sys/vmmeter.h>
+    #elif defined( Q_OS_DARWIN )
+        #include <mach/mach.h>
+    #endif
 #elif defined( Q_OS_WIN )
-    // Microsoft Windows systems
     #include <windows.h>
-#elif defined( Q_OS_UNIX )
-    // Unix-like systems: Linux, BSD and SysV
-    #include <unistd.h>
 #else
     #error "System not supported"
 #endif
@@ -22,8 +22,12 @@
 namespace MemOps {
 
 const size_t availableMemory() {
-#if defined( Q_OS_DARWIN )
-    // Darwin-based systems: macOS, iOS, watchOS and tvOS.
+#if defined( Q_OS_WIN )
+    MEMORYSTATUSEX mem_statexs;
+    mem_statexs.dwLength = sizeof( mem_statexs );
+    GlobalMemoryStatusEx( &mem_statexs );
+    return static_cast<size_t>( mem_statexs.ullAvailPhys );
+#elif defined( Q_OS_DARWIN )
     mach_msg_type_number_t count{ HOST_VM_INFO_COUNT };
     vm_statistics_data_t vmstat;
     if ( host_statistics( mach_host_self(), HOST_VM_INFO, (host_info_t)&vmstat, &count ) != KERN_SUCCESS ) {
@@ -35,14 +39,19 @@ const size_t availableMemory() {
         return 0ul;
     }
     return static_cast<size_t>( n_pages ) * static_cast<size_t>( page_size );
-#elif defined( Q_OS_WIN )
-    // Microsoft Windows systems
-    MEMORYSTATUSEX mem_statexs;
-    mem_statexs.dwLength = sizeof( mem_statexs );
-    GlobalMemoryStatusEx( &mem_statexs );
-    return static_cast<size_t>( mem_statexs.ullAvailPhys );
+#elif defined( Q_OS_BSD4 )
+    vmtotal vmt;
+    u_int page_size;
+    size_t vmt_size{ sizeof(vmt) };
+    size_t uint_size{ sizeof(page_size) };
+    if ( sysctlbyname("vm.vmtotal", &vmt, &vmt_size, NULL, 0) < 0 ) {
+        throw GenericException("Failed to get vmtotal", true);
+    }
+    if ( sysctlbyname("vm.stats.vm.v_page_size", &page_size, &uint_size, NULL, 0) < 0 ) {
+        throw GenericException("Failed to get v_page_size", true);
+    }
+    return vmt.t_free * static_cast<size_t>( page_size );
 #elif defined( Q_OS_UNIX )
-    // Unix-like systems: Linux, BSD and SysV
     long n_pages{ sysconf( _SC_AVPHYS_PAGES ) };
     long page_size{ sysconf( _SC_PAGE_SIZE ) };
     if ( n_pages < 0l || page_size < 0l ) {
