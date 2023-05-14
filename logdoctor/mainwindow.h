0861450fd115130e702492bac107854334564bb5
Valentino Orlandi <valentinox@disroot.org>
2023-05-14 14:27:12 +0200

    Added CustomTreeWidgetItem
    Allows "correctly" sorting log files list by name/size

--- logdoctor/mainwindow.h
+++ logdoctor/mainwindow.h
@@ -867,4 +867,45 @@ private:
 };
 
 
+
+class CustomTreeWidgetItem : public QTreeWidgetItem {
+public:
+    CustomTreeWidgetItem(QTreeWidget*parent=nullptr):QTreeWidgetItem(parent){}
+    bool operator < (const QTreeWidgetItem& other) const {
+        const int sort_column{ this->treeWidget()->sortColumn() };
+        const std::string s_str{ this->text(sort_column).toStdString() };
+        const std::string o_str{ other.text(sort_column).toStdString() };
+        if ( sort_column == 0 ) {
+            // sort by file name
+            const size_t s_sz{ s_str.size() - 1ul };
+            const size_t o_sz{ o_str.size() - 1ul };
+            if ( s_sz != o_sz ) {
+                return s_sz < o_sz;
+            }
+            for (size_t i{0ul}; i<=s_sz; i++) {
+                if ( s_str[i] != o_str[i] ) {
+                    return s_str[i] < o_str[i];
+                }
+            }
+        } else {
+            // sort by file size
+            const std::unordered_map<std::string,int> cmp{
+                {"B",  0},
+                {"KiB",1},
+                {"MiB",2}
+            };
+            std::vector<std::string> s_items;
+            StringOps::splitrip( s_items, s_str, ' ' );
+            std::vector<std::string> o_items;
+            StringOps::splitrip( o_items, o_str, ' ' );
+            if ( s_items[1] != o_items[1] ) {
+                return cmp.at( s_items[1] ) < cmp.at( o_items[1] );
+            }
+            return std::stof( s_items[0] ) < std::stof( o_items[0] );
+        }
+        return true;
+    }
+};
+
+
 #endif // MAINWINDOW_H
