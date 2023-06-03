fb2d97553c810f471501e76c2eeb84aff2024422
Valentino Orlandi <valentinox@disroot.org>
2023-06-03 17:21:21 +0200

    Moved WebServers IDs to defines folder
    Morphed from classes members to macros

---
+++ logdoctor/defines/web_servers.h
@@ -0,0 +1,11 @@
+#ifndef WEB_SERVERS_H
+#define WEB_SERVERS_H
+
+//! ID of the Apache2 Web Server
+#define APACHE_ID 11u
+//! ID of the Nginx Web Server
+#define NGINX_ID 12u
+//! ID of the IIS Web Server
+#define IIS_ID 13u
+
+#endif // WEB_SERVERS_H
