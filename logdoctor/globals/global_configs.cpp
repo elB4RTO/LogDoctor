fbb3786f73970f29c6b550a58e61f1bc9fd58465
Valentino Orlandi <valentinox@disroot.org>
2024-02-05 21:01:05 +0100

    Fix
    Fixed icons not updating at startup with native theme is system theme is
    dark

--- logdoctor/globals/global_configs.cpp
+++ logdoctor/globals/global_configs.cpp
@@ -11,6 +11,6 @@ WindowTheme window_theme = WindowTheme::Native;
 IconsTheme  icons_theme  = IconsTheme::Auto;
 ChartsTheme charts_theme = ChartsTheme::Light;
 
-QString icons_set = "light_native";
+QString icons_set = "";
 
 } // namespace GlobalConfigs
