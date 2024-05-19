08a12f543073cbbd7957832b9e42d7e608bd6c3a
Valentino Orlandi <valentinox@disroot.org>
2024-05-19 19:35:40 +0200

    Introduced PathHandler class
    Wrapper class to enforce checking paths before accessing them

---
+++ logdoctor/modules/security/path.cpp
@@ -0,0 +1,109 @@
+
+#include "path.h"
+
+#include "globals/security.h"
+
+#include "modules/exceptions.h"
+#include "modules/dialogs.h"
+
+
+using reason_t = InvalidPath::Reason;
+using components_t = std::vector<path_t>;
+using invalid_t = std::optional<std::tuple<path_t,reason_t>>;
+
+
+namespace /*private*/
+{
+
+path_t makeAbsolute( path_t&& path )
+{
+    return std::filesystem::absolute( path );
+}
+
+components_t getComponents( const path_t& path )
+{
+    path_t path_slice;
+    components_t components;
+    for ( const auto& component : path ) {
+        path_slice /= component;
+        components.push_back( path_slice );
+    }
+    return components;
+}
+
+invalid_t checkIsValid( const components_t& components )
+{
+    const bool symlink_denied{ ! GlobalConfigs::Security::follow_symlinks };
+
+    for ( const path_t& component : components ) {
+        if ( ! std::filesystem::exists( component ) ) {
+            return std::make_tuple( component, reason_t::NotExists );
+        } else if ( symlink_denied && std::filesystem::is_symlink( component ) ) {
+            return std::make_tuple( component, reason_t::IsSymlink );
+        }
+    }
+    return std::nullopt;
+}
+
+} // namespace (private)
+
+
+PathHandler::PathHandler( path_t&& path_ )
+    : path{std::move(path_)}, components{getComponents(this->path)}
+{
+}
+
+PathHandler::PathHandler( const std::string& path_ )
+    : path{makeAbsolute(path_)}, components{getComponents(this->path)}
+{
+}
+
+PathHandler::PathHandler( std::string&& path_ )
+    : path{makeAbsolute(std::move(path_))}, components{getComponents(this->path)}
+{
+}
+
+PathHandler PathHandler::operator/( const std::string& path_ ) const
+{
+    return PathHandler( this->path / path_ );
+}
+
+std::string PathHandler::toString() const
+{
+    return this->path.string();
+}
+
+std::expected<path_t, InvalidPath> PathHandler::getPath() const
+{
+    const invalid_t invalid_reason{ checkIsValid( this->components ) };
+    if ( invalid_reason.has_value() ) {
+        const auto [c,r]{ invalid_reason.value() };
+        return std::unexpected(InvalidPath( this->path, c, r ));
+    }
+    return this->path;
+}
+
+const path_t& PathHandler::getPathUnchecked() const
+{
+    return this->path;
+}
+
+const path_t PathHandler::getParentUnchecked() const
+{
+    return this->path.parent_path();
+}
+
+
+void InvalidPath::showDialogMessage() const
+{
+    const QString fp( this->full_path.c_str() );
+    const QString ic( this->invalid_component.c_str() );
+    switch ( this->reason ) {
+        case Reason::NotExists:
+            DialogSec::errPathNotExists( fp, ic );
+            break;
+        case Reason::IsSymlink:
+            DialogSec::errPathHasSymlink( fp, ic );
+            break;
+    }
+}
---
+++ logdoctor/modules/security/path.h
@@ -0,0 +1,82 @@
+#ifndef LOGDOCTOR__MODULES__SECURITY__PATH_H
+#define LOGDOCTOR__MODULES__SECURITY__PATH_H
+
+
+#include <expected>
+#include <filesystem>
+#include <vector>
+
+
+using path_t = std::filesystem::path;
+
+
+class InvalidPath;
+
+
+//! PathHandler
+/*!
+    Handles a path, enforcing checks when access to the resource is requested
+*/
+class PathHandler final
+{
+    path_t path;
+    std::vector<path_t> components;
+
+public:
+    PathHandler() = default;
+    PathHandler( PathHandler&& ) = default;
+    PathHandler( const PathHandler& ) = default;
+    PathHandler& operator=( PathHandler&& ) = default;
+    PathHandler& operator=( const PathHandler& ) = default;
+
+    PathHandler( path_t&& path_ );
+    PathHandler( const std::string& path_ );
+    PathHandler( std::string&& path_ );
+
+    PathHandler operator/( const std::string& path_ ) const;
+
+    //! Convenience function that returns the path as string, without checks
+    std::string toString() const;
+
+    //! Returns the path if it is valid, throws otherwise
+    /*!
+        Checks every component of the path to determine whether it is valid.
+        For a path to be valid, all of its components must exist on the filesystem
+        and they must follow the security configuration about the symlinks.
+        \throw VoidException
+    */
+    std::expected<path_t, InvalidPath> getPath() const;
+
+    //! Convenience function that just returns the path, skipping all the checks
+    const path_t& getPathUnchecked() const;
+
+    //! Convenience function that just returns the parent path, skipping all the checks
+    const path_t getParentUnchecked() const;
+};
+
+
+struct InvalidPath final
+{
+    enum class Reason {
+        NotExists,
+        IsSymlink,
+    };
+
+    const path_t full_path;
+    const path_t invalid_component;
+    const Reason reason;
+
+    InvalidPath( const path_t& p, const path_t& c, const Reason r )
+    : full_path{p}, invalid_component{c}, reason{r} {}
+
+    bool isReasonExistence() const
+    { return reason == Reason::NotExists; }
+
+    bool isReasonSymlink() const
+    { return reason == Reason::IsSymlink; }
+
+    void showDialogMessage() const;
+};
+
+
+#endif // LOGDOCTOR__MODULES__SECURITY__PATH_H
