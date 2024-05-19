0ac75c0f122f2d2a88ec6afefd558e36d539edb3
Valentino Orlandi <valentinox@disroot.org>
2024-05-19 19:32:09 +0200

    Updated GlobalConfigs namespace
    Added Security nested namespace

---
+++ logdoctor/globals/security.cpp
@@ -0,0 +1,10 @@
+
+#include "security.h"
+
+
+namespace GlobalConfigs::Security
+{
+
+bool follow_symlinks = false;
+
+} // namespace GlobalConfigs::Security
---
+++ logdoctor/globals/security.h
@@ -0,0 +1,17 @@
+#ifndef LOGDOCTOR__GLOBALS__SECURITY_H
+#define LOGDOCTOR__GLOBALS__SECURITY_H
+
+
+//! Security
+/*!
+    Globally shared configurations about security
+*/
+namespace GlobalConfigs::Security
+{
+
+extern bool follow_symlinks;
+
+} // namespace SecurityOptions::Security
+
+
+#endif // LOGDOCTOR__GLOBALS__SECURITY_H
